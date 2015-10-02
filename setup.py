1361a891bbdb0348a3817c5e7f1910537608dd27
Alexandre Barachant <alexandre.barachant@gmail.com>
2015-10-02 16:05:17 -0400

    travis 4

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(name='pyriemann',
-      version='0.2.1',
+      version='0.2.2',
       description='Riemannian Geometry for python',
       url='',
       author='Alexandre Barachant',
       author_email='alexandre.barachant@gmail.com',
       license='BSD (3-clause)',
       packages=find_packages(),
-      install_requires=['numpy','scipy','scikit-learn', 'joblib'],
+      # install_requires=['numpy', 'scipy', 'scikit-learn',  'joblib', 'pandas'],
       zip_safe=False)
