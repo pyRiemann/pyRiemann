baf981da8416a66a67965b4f93816d118fe342a6
Alexandre Barachant <alexandre.barachant@gmail.com>
2015-10-02 14:07:18 -0400

    Flake 8

--- pyriemann/clustering.py
+++ pyriemann/clustering.py
@@ -1,11 +1,11 @@
+"""Clustering functions."""
 import numpy
-from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin, ClusterMixin
+from sklearn.base import (BaseEstimator, ClassifierMixin, TransformerMixin,
+                          ClusterMixin)
 from sklearn.cluster.k_means_ import _init_centroids
 from sklearn.externals.joblib import Parallel
 from sklearn.externals.joblib import delayed
 
-from .utils.mean import mean_covariance
-from .utils.distance import distance
 from .classification import MDM
 
 #######################################################################
@@ -44,19 +44,14 @@ def _fit_single(
     return labels, inertia, mdm
 
 
-#######################################################################
 class Kmeans(BaseEstimator, ClassifierMixin, ClusterMixin, TransformerMixin):
 
-    def __init__(
-            self,
-            n_clusters=2,
-            max_iter=100,
-            metric='riemann',
-            random_state=None,
-            init='random',
-            n_init=10,
-            n_jobs=1,
-            tol=1e-4):
+    """Kmeans unsupervised clustering."""
+
+    def __init__(self, n_clusters=2, max_iter=100, metric='riemann',
+                 random_state=None, init='random', n_init=10, n_jobs=1,
+                 tol=1e-4):
+        """Init."""
         self.metric = metric
         self.n_clusters = n_clusters
         self.max_iter = max_iter
@@ -68,6 +63,7 @@ class Kmeans(BaseEstimator, ClassifierMixin, ClusterMixin, TransformerMixin):
         self.n_jobs = n_jobs
 
     def fit(self, X, y=None):
+        """Fit."""
         if (self.init is not 'random') | (self.n_init == 1):
             # no need to iterate if init is not random
             labels, inertia, mdm = _fit_single(X, y,
@@ -111,40 +107,45 @@ class Kmeans(BaseEstimator, ClassifierMixin, ClusterMixin, TransformerMixin):
             inertial = inertia[best]
 
         self.mdm = mdm
-        self.inertia = inertia
+        self.inertia = inertial
         self.labels_ = labels
 
         return self
 
     def predict(self, X):
+        """predict."""
         return self.mdm.predict(X)
 
     def transform(self, X):
+        """transform."""
         return self.mdm.transform(X)
 
     def covmeans(self):
+        """return centroid."""
         return self.mdm.covmeans
-
-#######################################################################
 
 
 class KmeansPerClassTransform(BaseEstimator, TransformerMixin):
 
+    """Run kmeans for each class."""
+
     def __init__(self, n_clusters=2, **params):
+        """Init."""
         params['n_clusters'] = n_clusters
         self.km = Kmeans(**params)
         self.metric = self.km.metric
         self.covmeans = []
 
     def fit(self, X, y):
+        """fit."""
         self.classes = numpy.unique(y)
-        nclasses = len(self.classes)
         for c in self.classes:
             self.km.fit(X[y == c])
             self.covmeans.extend(self.km.covmeans())
         return self
 
     def transform(self, X):
+        """transform."""
         mdm = MDM(metric=self.metric)
         mdm.covmeans = self.covmeans
         return mdm._predict_distances(X)
