64676ec8b8280c14f7e73f27c5b1055dc7c07e89
Alexandre Barachant <alexandre.barachant@gmail.com>
2015-10-04 17:50:20 -0400

    Docstring for classification

--- pyriemann/classification.py
+++ pyriemann/classification.py
@@ -1,3 +1,4 @@
+"""Module for classification function."""
 import numpy
 from sklearn.base import BaseEstimator, ClassifierMixin, TransformerMixin
 from joblib import Parallel, delayed
@@ -6,12 +7,59 @@ from .utils.mean import mean_covariance
 from .utils.distance import distance
 from .tangentspace import FGDA
 
-#######################################################################
 
 class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
 
-    def __init__(self, metric='riemann',n_jobs=1):
+    """Classification by Minimum Distance to Mean.
 
+    Classification by nearest centroid. For each of the given classes, a
+    centroid is estimated according to the chosen metric. Then, for each new
+    point, the class is affected according to the nearest centroid.
+
+    Parameters
+    ----------
+    metric : string | dict (default: 'riemann')
+        The type of metric used for centroid and distance estimation.
+        see `mean_covariance` for the list of supported metric.
+        the metric could be a dict with two keys, `mean` and `distance` in
+        order to pass different metric for the centroid estimation and the
+        distance estimation. Typical usecase is to pass 'logeuclid' metric for
+        the mean in order to boost the computional speed and 'riemann' for the
+        distance in order to keep the good sensitivity for the classification.
+    n_jobs : int, (default: 1)
+        The number of jobs to use for the computation. This works by computing
+        each of the class centroid in parallel.
+        If -1 all CPUs are used. If 1 is given, no parallel computing code is
+        used at all, which is useful for debugging. For n_jobs below -1,
+        (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs but one
+        are used.
+
+    Attributes
+    ----------
+    covmeans : list
+        the class centroids.
+    classes : list
+        list of classes.
+
+    See Also
+    --------
+    Kmeans
+    FgMDM
+
+    References
+    ----------
+    [1] A. Barachant, S. Bonnet, M. Congedo and C. Jutten, "Multiclass
+    Brain-Computer Interface Classification by Riemannian Geometry," in IEEE
+    Transactions on Biomedical Engineering, vol. 59, no. 4, p. 920-928, 2012.
+
+    [2] A. Barachant, S. Bonnet, M. Congedo and C. Jutten, "Riemannian geometry
+    applied to BCI classification", 9th International Conference Latent
+    Variable Analysis and Signal Separation (LVA/ICA 2010), LNCS vol. 6365,
+    2010, p. 629-636.
+    """
+
+    def __init__(self, metric='riemann', n_jobs=1):
+        """Init."""
         # store params for cloning purpose
         self.metric = metric
         self.n_jobs = n_jobs
@@ -33,7 +81,20 @@ class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
             raise TypeError('metric must be dict or str')
 
     def fit(self, X, y):
+        """Fit (estimates) the centroids.
 
+        Parameters
+        ----------
+        X : ndarray, shape (n_trials, n_channels, n_channels)
+            ndarray of SPD matrices.
+        y : ndarray shape (n_trials, 1)
+            labels corresponding to each trial.
+
+        Returns
+        -------
+        self : MDM instance
+            The MDM instance.
+        """
         self.classes = numpy.unique(y)
 
         self.covmeans = []
@@ -48,6 +109,7 @@ class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
         return self
 
     def _predict_distances(self, covtest):
+        """Helper to predict the distance. equivalent to transform."""
         Nc = len(self.covmeans)
 
         if self.n_jobs == 1:
@@ -59,35 +121,163 @@ class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
         dist = numpy.concatenate(dist, axis=1)
         return dist
 
-
-
     def predict(self, covtest):
+        """get the predictions.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_trials, n_channels, n_channels)
+            ndarray of SPD matrices.
+
+        Returns
+        -------
+        pred : ndarray of int, shape (n_trials, 1)
+            the prediction for each trials according to the closest centroid.
+        """
         dist = self._predict_distances(covtest)
         return self.classes[dist.argmin(axis=1)]
 
     def transform(self, X):
+        """get the distance to each centroid.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_trials, n_channels, n_channels)
+            ndarray of SPD matrices.
+
+        Returns
+        -------
+        dist : ndarray, shape (n_trials, n_cluster)
+            the distance to each centroid according to the metric.
+        """
         return self._predict_distances(X)
 
     def fit_predict(self, X, y):
+        """Fit and predict in one function."""
         self.fit(X, y)
         return self.predict(X)
 
-#######################################################################
 
+class FgMDM(BaseEstimator, ClassifierMixin, TransformerMixin):
 
-class FgMDM(BaseEstimator, ClassifierMixin):
+    """Classification by Minimum Distance to Mean with geodesic filtering.
 
-    def __init__(self, metric='riemann', tsupdate=False):
+    Apply geodesic filtering described in [1], and classify using MDM algorithm
+    The geodesic filtering is achieved in tangent space with a Linear
+    Discriminant Analysis, then data are projected back to the manifold and
+    classifier with a regular mdm.
+    This is basically a pipeline of FGDA and MDM
+
+    Parameters
+    ----------
+    metric : string | dict (default: 'riemann')
+        The type of metric used for centroid and distance estimation.
+        see `mean_covariance` for the list of supported metric.
+        the metric could be a dict with two keys, `mean` and `distance` in
+        order to pass different metric for the centroid estimation and the
+        distance estimation. Typical usecase is to pass 'logeuclid' metric for
+        the mean in order to boost the computional speed and 'riemann' for the
+        distance in order to keep the good sensitivity for the classification.
+    tsupdate : bool (default False)
+        Activate tangent space update for covariante shift correction between
+        training and test, as described in [2]. This is not compatible with
+        online implementation. Performance are better when the number of trials
+        for prediction is higher.
+    n_jobs : int, (default: 1)
+        The number of jobs to use for the computation. This works by computing
+        each of the class centroid in parallel.
+        If -1 all CPUs are used. If 1 is given, no parallel computing code is
+        used at all, which is useful for debugging. For n_jobs below -1,
+        (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs but one
+        are used.
+
+    See Also
+    --------
+    MDM
+    FGDA
+    TangentSpace
+
+    References
+    ----------
+    [1] A. Barachant, S. Bonnet, M. Congedo and C. Jutten, "Riemannian geometry
+    applied to BCI classification", 9th International Conference Latent
+    Variable Analysis and Signal Separation (LVA/ICA 2010), LNCS vol. 6365,
+    2010, p. 629-636.
+
+    [2] A. Barachant, S. Bonnet, M. Congedo and C. Jutten, "Classification of
+    covariance matrices using a Riemannian-based kernel for BCI applications",
+    in NeuroComputing, vol. 112, p. 172-178, 2013.
+    """
+
+    def __init__(self, metric='riemann', tsupdate=False, n_jobs=1):
+        """Init."""
         self.metric = metric
+        self.n_jobs = n_jobs
         self.tsupdate = tsupdate
-        self._mdm = MDM(metric=self.metric)
-        self._fgda = FGDA(metric=self.metric, tsupdate=self.tsupdate)
+
+        if isinstance(metric, str):
+            self.metric_mean = metric
+
+        elif isinstance(metric, dict):
+            # check keys
+            for key in ['mean', 'distance']:
+                if key not in metric.keys():
+                    raise KeyError('metric must contain "mean" and "distance"')
+
+            self.metric_mean = metric['mean']
+
+        else:
+            raise TypeError('metric must be dict or str')
 
     def fit(self, X, y):
+        """Fit FgMDM.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_trials, n_channels, n_channels)
+            ndarray of SPD matrices.
+        y : ndarray shape (n_trials, 1)
+            labels corresponding to each trial.
+
+        Returns
+        -------
+        self : FgMDM instance
+            The FgMDM instance.
+        """
+        self._mdm = MDM(metric=self.metric, n_jobs=self.n_jobs)
+        self._fgda = FGDA(metric=self.metric_mean, tsupdate=self.tsupdate)
         cov = self._fgda.fit_transform(X, y)
         self._mdm.fit(cov, y)
         return self
 
     def predict(self, X):
+        """get the predictions.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_trials, n_channels, n_channels)
+            ndarray of SPD matrices.
+
+        Returns
+        -------
+        pred : ndarray of int, shape (n_trials, 1)
+            the prediction for each trials according to the closest centroid.
+        """
         cov = self._fgda.transform(X)
         return self._mdm.predict(cov)
+
+    def transform(self, X):
+        """get the distance to each centroid.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_trials, n_channels, n_channels)
+            ndarray of SPD matrices.
+
+        Returns
+        -------
+        dist : ndarray, shape (n_trials, n_cluster)
+            the distance to each centroid according to the metric.
+        """
+        cov = self._fgda.transform(X)
+        return self._mdm.transform(cov)
