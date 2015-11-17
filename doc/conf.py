80c2b37b4efbac5f9ff293a9362a6bbbf6805cd1
Alexandre Barachant <alexandre.barachant@gmail.com>
2015-11-17 17:16:23 -0500

    exemple galery

--- doc/conf.py
+++ doc/conf.py
@@ -46,7 +46,7 @@ extensions = [
 ]
 
 sphinx_gallery_conf = {
-    'examples_dirs' : 'examples'}
+    'examples_dirs' : '../examples'}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
