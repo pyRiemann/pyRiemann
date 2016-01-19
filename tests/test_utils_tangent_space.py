97284a71eec47ed0555f43d0af7a21ea057e1e1b
Alexandre Barachant <alexandre.barachant@gmail.com>
2016-01-18 19:51:43 -0500

    Fix travis

--- tests/test_utils_tangent_space.py
+++ tests/test_utils_tangent_space.py
@@ -5,22 +5,23 @@ from pyriemann.utils.tangentspace import tangent_space,untangent_space
 
 def generate_cov(Nt,Ne):
     """Generate a set of cavariances matrices for test purpose"""
-    diags = 1.0+0.1*np.random.randn(Nt,Ne)
+    np.random.seed(1234)
+    diags = 2.0+0.1*np.random.randn(Nt,Ne)
     covmats = np.empty((Nt,Ne,Ne))
     for i in range(Nt):
         covmats[i] = np.diag(diags[i])
     return covmats
-    
+
 def test_tangent_space():
     """Test tangent space projection"""
     C = generate_cov(10,3)
     T = tangent_space(C,np.eye(3))
-    
+
 def test_untangent_space():
     """Test untangent space projection"""
     T = np.random.randn(10,6)
     covmats = untangent_space(T,np.eye(3))
-    
+
 def test_tangent_and_untangent_space():
     """Test tangent space projection and retro-projection should be the same"""
     C = generate_cov(10,3)
