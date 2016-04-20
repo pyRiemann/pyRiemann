6764d76af0f016d2ac6ad6c1bfc34e17d7f5bab4
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2016-04-20 09:39:35 +0200

    covering all functions of classification: TSclassifier, kNN

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -1,12 +1,12 @@
 import numpy as np
 from nose.tools import assert_raises
-from pyriemann.classification import MDM,FgMDM
+from pyriemann.classification import MDM, FgMDM
 
 def generate_cov(Nt,Ne):
     """Generate a set of cavariances matrices for test purpose"""
     np.random.seed(1234)
-    diags = 2.0+0.1*np.random.randn(Nt,Ne)
-    covmats = np.empty((Nt,Ne,Ne))
+    diags = 2. + 0.1 * np.random.randn(Nt, Ne)
+    covmats = np.empty(shape=(Nt, Ne, Ne))
     for i in range(Nt):
         covmats[i] = np.diag(diags[i])
     return covmats
@@ -38,13 +38,17 @@ def test_MDM_predict():
     mdm = MDM(metric='riemann')
     mdm.fit(covset,labels)
     mdm.predict(covset)
+    mdm = MDM(n_jobs=2)
+    mdm.fit(covset,labels)
+    mdm.predict(covset)
+    mdm.predict_proba(covset)
 
 def test_MDM_fit_predict():
     """Test Fit & predict of MDM"""
     covset = generate_cov(100,3)
     labels = np.array([0,1]).repeat(50)
     mdm = MDM(metric='riemann')
-    mdm.fit_predict(covset,labels)
+    mdm.fit_predict(covset, labels)
 
 def test_MDM_transform():
     """Test transform of MDM"""
@@ -58,13 +62,18 @@ def test_MDM_transform():
 def test_FgMDM_init():
     """Test init of FgMDM"""
     mdm = FgMDM(metric='riemann')
+    mdm = FgMDM(metric={'mean':'logeuclid', 'distance':'riemann'})
 
-def test_FgMDM_fit():
-    """Test Fit of FgMDM"""
+    assert_raises(KeyError, FgMDM, metric={'foo':'bar'})
+    assert_raises(TypeError, FgMDM, metric=42)
+
+def test_FgMDM_fit_transform():
+    """Test fit and transform of FgMDM"""
     covset = generate_cov(100,3)
     labels = np.array([0,1]).repeat(50)
     mdm = FgMDM(metric='riemann')
     mdm.fit(covset,labels)
+    mdm.transform(covset)
 
 def test_FgMDM_predict():
     """Test prediction of FgMDM"""
@@ -73,3 +82,47 @@ def test_FgMDM_predict():
     mdm = FgMDM(metric='riemann')
     mdm.fit(covset,labels)
     mdm.predict(covset)
+
+
+def test_TSc_init():
+    """Test init of TSClassifier"""
+    tsc = TSclassifier(metric='riemann')
+    tsc = TSclassifier(tsupdate=True)
+    assert_raises(TypeError, TSclassifier, clf=42)
+
+def test_TSc_fit_transform():
+    """Test fit and transform of TSClassifier"""
+    covset = generate_cov(100,3)
+    labels = np.array([0,1]).repeat(50)
+    tsc = TSclassifier(metric='riemann')
+    tsc.fit(covset,labels)
+    tsc.transform(covset)
+
+def test_TSc_predict():
+    """Test prediction of TSClassifier"""
+    covset = generate_cov(100,3)
+    labels = np.array([0,1]).repeat(50)
+    ts = TSclassifier(metric='riemann')
+    tsc.fit(covset,labels)
+    tsc.predict(covset)
+    tsc.predict_proba(covset)
+
+
+def test_kNN_init():
+    """Test init of KNearestNeighbor"""
+    knn = KNearestNeighbor(metric='riemann')
+
+def test_kNN_fit_transform():
+    """Test fit of KNearestNeighbor"""
+    covset = generate_cov(100,3)
+    labels = np.array([0,1]).repeat(50)
+    knn = KNearestNeighbor(metric='riemann')
+    knn.fit(covset,labels)
+
+def test_kNN_predict():
+    """Test prediction of KNearestNeighbor"""
+    covset = generate_cov(100,3)
+    labels = np.array([0,1]).repeat(50)
+    ts = KNearestNeighbor(metric='riemann')
+    knn.fit(covset,labels)
+    knn.predict(covset)
