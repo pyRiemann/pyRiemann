e3c9d665b48275bb95751ee07e9eb13163fc4748
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2016-04-20 09:47:58 +0200

    PEP8 conformation for test clustering

--- tests/test_clustering.py
+++ tests/test_clustering.py
@@ -1,72 +1,83 @@
 import numpy as np
-from pyriemann.clustering import Kmeans,KmeansPerClassTransform
+from pyriemann.clustering import Kmeans, KmeansPerClassTransform
 
-def generate_cov(Nt,Ne):
+
+def generate_cov(Nt, Ne):
     """Generate a set of cavariances matrices for test purpose"""
-    diags = 1.0+0.1*np.random.randn(Nt,Ne)
-    covmats = np.empty((Nt,Ne,Ne))
+    diags = 1.0+0.1*np.random.randn(Nt, Ne)
+    covmats = np.empty((Nt, Ne, Ne))
     for i in range(Nt):
         covmats[i] = np.diag(diags[i])
     return covmats
 
+
 def test_Kmeans_init():
     """Test init of Kmeans"""
     km = Kmeans(2)
-    
+
+
 def test_Kmeans_fit():
     """Test Fit of Kmeans"""
-    covset = generate_cov(20,3)
+    covset = generate_cov(20, 3)
     km = Kmeans(2)
     km.fit(covset)
-    
+
+
 def test_Kmeans_fit_with_init():
     """Test Fit of Kmeans wit matric initialization"""
-    covset = generate_cov(20,3)
-    km = Kmeans(2,init=covset[0:2])
+    covset = generate_cov(20, 3)
+    km = Kmeans(2, init=covset[0:2])
     km.fit(covset)
+
 
 def test_Kmeans_fit_with_y():
     """Test Fit of Kmeans with a given y"""
-    covset = generate_cov(20,3)
-    labels = np.array([0,1]).repeat(10)
+    covset = generate_cov(20, 3)
+    labels = np.array([0, 1]).repeat(10)
     km = Kmeans(2)
-    km.fit(covset,y=labels)
+    km.fit(covset, y=labels)
+
 
 def test_Kmeans_fit_parallel():
     """Test Fit of Kmeans using paralell"""
-    covset = generate_cov(20,3)
-    km = Kmeans(2,n_jobs=2)
+    covset = generate_cov(20, 3)
+    km = Kmeans(2, n_jobs=2)
     km.fit(covset)
+
 
 def test_Kmeans_predict():
     """Test prediction of Kmeans"""
-    covset = generate_cov(20,3)
+    covset = generate_cov(20, 3)
     km = Kmeans(2)
     km.fit(covset)
     km.predict(covset)
-    
+
+
 def test_Kmeans_transform():
     """Test transform of Kmeans"""
-    covset = generate_cov(20,3)
+    covset = generate_cov(20, 3)
     km = Kmeans(2)
     km.fit(covset)
     km.transform(covset)
-    
+
+
 def test_KmeansPCT_init():
     """Test init of Kmeans PCT"""
     km = KmeansPerClassTransform(2)
-    
+
+
 def test_KmeansPCT_fit():
     """Test Fit of Kmeans PCT"""
-    covset = generate_cov(20,3)
-    labels = np.array([0,1]).repeat(10)
+    covset = generate_cov(20, 3)
+    labels = np.array([0, 1]).repeat(10)
     km = KmeansPerClassTransform(2)
-    km.fit(covset,labels)
-    
+    km.fit(covset, labels)
+
+
 def test_KmeansPCT_transform():
     """Test Transform of Kmeans PCT"""
-    covset = generate_cov(20,3)
-    labels = np.array([0,1]).repeat(10)
+    covset = generate_cov(20, 3)
+    labels = np.array([0, 1]).repeat(10)
     km = KmeansPerClassTransform(2)
-    km.fit(covset,labels)
+    km.fit(covset, labels)
     km.transform(covset)
