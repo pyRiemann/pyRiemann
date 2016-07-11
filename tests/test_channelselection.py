c1ce24bc34a9a69a585ac219336cf4770d084629
kingjr <jeanremi.king+github@gmail.com>
2016-07-11 16:50:39 -0400

    ENH: flat channel detector

--- tests/test_channelselection.py
+++ tests/test_channelselection.py
@@ -1,6 +1,6 @@
 """Test for channel selection."""
 import numpy as np
-from pyriemann.channelselection import ElectrodeSelection
+from pyriemann.channelselection import ElectrodeSelection, FlatChannelRemover
 
 
 def generate_cov(Nt, Ne):
@@ -19,3 +19,13 @@ def test_ElectrodeSelection_transform():
     se = ElectrodeSelection()
     se.fit(covset, labels)
     se.transform(covset)
+
+
+def test_FlatChannelRemover():
+    X = np.random.rand(100, 10, 3)
+    X[:, 0, :] = 999
+    fcr = FlatChannelRemover()
+    fcr.fit(X)
+    np.testing.assert_array_equal(fcr.channels_, range(1, 10))
+    Xt = fcr.fit_transform(X)
+    np.testing.assert_array_equal(Xt, X[:, 1:, :])
