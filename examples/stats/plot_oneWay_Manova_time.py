a3715c8e871ee7feea38aec4b1cf87fe3767e451
Alexandre Barachant <alexandre.barachant@gmail.com>
2020-03-18 19:02:30 -0400

    fix rest of examples

--- examples/stats/plot_oneWay_Manova_time.py
+++ examples/stats/plot_oneWay_Manova_time.py
@@ -11,11 +11,10 @@ import seaborn as sns
 from time import time
 from pylab import plt
 
-from mne import Epochs, pick_types
+from mne import Epochs, pick_types, events_from_annotations
 from mne.io import concatenate_raws
 from mne.io.edf import read_raw_edf
 from mne.datasets import eegbci
-from mne.event import find_events
 
 from pyriemann.stats import PermutationDistance
 from pyriemann.estimation import Covariances
@@ -33,19 +32,29 @@ event_id = dict(hands=2, feet=3)
 subject = 1
 runs = [6, 10, 14]  # motor imagery: hands vs feet
 
-raw_files = [read_raw_edf(f, preload=True, verbose=False)
-             for f in eegbci.load_data(subject, runs)]
+raw_files = [
+    read_raw_edf(f, preload=True, verbose=False)
+    for f in eegbci.load_data(subject, runs)
+]
 raw = concatenate_raws(raw_files)
 
-events = find_events(raw, shortest_event=0, stim_channel='STI 014')
-picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False,
-                   exclude='bads')
+events, _ = events_from_annotations(raw, event_id=dict(T1=2, T2=3))
+picks = pick_types(
+    raw.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
 
 raw.filter(7., 35., method='iir', picks=picks)
 
-
-epochs = Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
-                baseline=None, preload=True, verbose=False)
+epochs = Epochs(
+    raw,
+    events,
+    event_id,
+    tmin,
+    tmax,
+    proj=True,
+    picks=picks,
+    baseline=None,
+    preload=True,
+    verbose=False)
 labels = epochs.events[:, -1] - 2
 
 # get epochs
@@ -58,18 +67,18 @@ epochs_data = epochs.get_data()
 covest = Covariances()
 
 Fs = 160
-window = 2*Fs
+window = 2 * Fs
 Nwindow = 20
 Ns = epochs_data.shape[2]
-step = int((Ns-window)/Nwindow)
-time_bins = range(0, Ns-window, step)
+step = int((Ns - window) / Nwindow)
+time_bins = range(0, Ns - window, step)
 
 pv = []
 Fv = []
 # For each frequency bin, estimate the stats
 t_init = time()
 for t in time_bins:
-    covmats = covest.fit_transform(epochs_data[:, ::1, t:(t+window)])
+    covmats = covest.fit_transform(epochs_data[:, ::1, t:(t + window)])
     p_test = PermutationDistance(1000, metric='riemann', mode='pairwise')
     p, F = p_test.test(covmats, labels, verbose=False)
     pv.append(p)
@@ -78,17 +87,17 @@ duration = time() - t_init
 # plot result
 fig, axes = plt.subplots(1, 1, figsize=[6, 3], sharey=True)
 sig = 0.05
-times = np.array(time_bins)/float(Fs) + tmin
+times = np.array(time_bins) / float(Fs) + tmin
 
 axes.plot(times, Fv, lw=2, c='k')
 plt.xlabel('Time (sec)')
 plt.ylabel('Score')
 
 a = np.where(np.diff(np.array(pv) < sig))[0]
-a = a.reshape(int(len(a)/2), 2)
-st = (times[1] - times[0])/2.0
+a = a.reshape(int(len(a) / 2), 2)
+st = (times[1] - times[0]) / 2.0
 for p in a:
-    axes.axvspan(times[p[0]]-st, times[p[1]]+st, facecolor='g', alpha=0.5)
+    axes.axvspan(times[p[0]] - st, times[p[1]] + st, facecolor='g', alpha=0.5)
 axes.legend(['Score', 'p<%.2f' % sig])
 axes.set_title('Pairwise distance - %.1f sec.' % duration)
 
