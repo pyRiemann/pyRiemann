469660cfd4209721b3ca9e50b78fc5e063ce4eac
Alexandre Barachant <alexandre.barachant@gmail.com>
2020-03-18 18:56:43 -0400

    fix doc

--- examples/motor-imagery/plot_single.py
+++ examples/motor-imagery/plot_single.py
@@ -12,11 +12,10 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 
 # mne import
-from mne import Epochs, pick_types
+from mne import Epochs, pick_types, events_from_annotations
 from mne.io import concatenate_raws
 from mne.io.edf import read_raw_edf
 from mne.datasets import eegbci
-from mne.event import find_events
 from mne.decoding import CSP
 
 # pyriemann import
@@ -24,7 +23,7 @@ from pyriemann.classification import MDM, TSclassifier
 from pyriemann.estimation import Covariances
 
 # sklearn imports
-from sklearn.cross_validation import cross_val_score, KFold
+from sklearn.model_selection import cross_val_score, KFold
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 
@@ -38,31 +37,40 @@ event_id = dict(hands=2, feet=3)
 subject = 7
 runs = [6, 10, 14]  # motor imagery: hands vs feet
 
-raw_files = [read_raw_edf(f, preload=True) for f in eegbci.load_data(subject,
-             runs)]
+raw_files = [
+    read_raw_edf(f, preload=True) for f in eegbci.load_data(subject, runs)
+]
 raw = concatenate_raws(raw_files)
 
-picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False,
-                   exclude='bads')
+picks = pick_types(
+    raw.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
 # subsample elecs
 picks = picks[::2]
 
 # Apply band-pass filter
 raw.filter(7., 35., method='iir', picks=picks)
 
-events = find_events(raw, shortest_event=0, stim_channel='STI 014')
+events, _ = events_from_annotations(raw, event_id=dict(T1=2, T2=3))
 
 # Read epochs (train will be done only between 1 and 2s)
 # Testing will be done with a running classifier
-epochs = Epochs(raw, events, event_id, tmin, tmax, proj=True, picks=picks,
-                baseline=None, preload=True, verbose=False)
+epochs = Epochs(
+    raw,
+    events,
+    event_id,
+    tmin,
+    tmax,
+    proj=True,
+    picks=picks,
+    baseline=None,
+    preload=True,
+    verbose=False)
 labels = epochs.events[:, -1] - 2
 
-
 # cross validation
-cv = KFold(len(labels), 10, shuffle=True, random_state=42)
+cv = KFold(n_splits=10, random_state=42)
 # get epochs
-epochs_data_train = 1e6*epochs.get_data()
+epochs_data_train = 1e6 * epochs.get_data()
 
 # compute covariance matrices
 cov_data_train = Covariances().transform(epochs_data_train)
@@ -98,7 +106,6 @@ print("Tangent space Classification accuracy: %f / Chance level: %f" %
 lr = LogisticRegression()
 csp = CSP(n_components=4, reg='ledoit_wolf', log=True)
 
-
 clf = Pipeline([('CSP', csp), ('LogisticRegression', lr)])
 scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=1)
 
@@ -118,13 +125,13 @@ fig, axes = plt.subplots(1, 2, figsize=[8, 4])
 ch_names = [ch.replace('.', '') for ch in epochs.ch_names]
 
 df = pd.DataFrame(data=mdm.covmeans_[0], index=ch_names, columns=ch_names)
-g = sns.heatmap(df, ax=axes[0], square=True, cbar=False, xticklabels=2,
-                yticklabels=2)
+g = sns.heatmap(
+    df, ax=axes[0], square=True, cbar=False, xticklabels=2, yticklabels=2)
 g.set_title('Mean covariance - hands')
 
 df = pd.DataFrame(data=mdm.covmeans_[1], index=ch_names, columns=ch_names)
-g = sns.heatmap(df, ax=axes[1], square=True, cbar=False, xticklabels=2,
-                yticklabels=2)
+g = sns.heatmap(
+    df, ax=axes[1], square=True, cbar=False, xticklabels=2, yticklabels=2)
 plt.xticks(rotation='vertical')
 plt.yticks(rotation='horizontal')
 g.set_title('Mean covariance - feets')
