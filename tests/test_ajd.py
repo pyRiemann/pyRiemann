ff7d556b7c2bacf5c1587f93a2bbcdddeaa360e9
qbarthelemy <q.barthelemy@gmail.com>
2020-11-05 21:12:44 +0100

    add weights in ajd_pham

--- tests/test_ajd.py
+++ tests/test_ajd.py
@@ -1,7 +1,8 @@
 from numpy.testing import assert_array_almost_equal, assert_array_equal
+from nose.tools import assert_raises
 import numpy as np
 
-from pyriemann.utils.ajd import rjd, ajd_pham, uwedge
+from pyriemann.utils.ajd import _get_sample_weight, rjd, ajd_pham, uwedge
 
 
 def generate_cov(Nt, Ne):
@@ -16,6 +17,15 @@ def generate_cov(Nt, Ne):
     return covmats, diags, A
 
 
+def test_get_sample_weight():
+    """Test get_sample_weight"""
+    Nt = 100
+    covmats, diags, A = generate_cov(Nt, 3)
+    w = np.random.randn(Nt)
+    assert_raises(ValueError, _get_sample_weight, w, covmats) # non-positive
+    assert_raises(ValueError, _get_sample_weight, w[:Nt//2], covmats)
+
+
 def test_rjd():
     """Test rjd"""
     covmats, diags, A = generate_cov(100, 3)
@@ -24,8 +34,14 @@ def test_rjd():
 
 def test_pham():
     """Test pham's ajd"""
-    covmats, diags, A = generate_cov(100, 3)
+    Nt = 100
+    covmats, diags, A = generate_cov(Nt, 3)
     V, D = ajd_pham(covmats)
+
+    w = 5 * np.ones(Nt)
+    Vw, Dw = ajd_pham(covmats, sample_weight=w)
+    assert_array_equal(V, Vw)
+    assert_array_equal(D, Dw)
 
 
 def test_uwedge():
