ac93e34c8b52a19f9da946e12eddf7be024a49d4
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-06-07 22:14:19 +0200

    fix sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -283,7 +283,7 @@ latex_documents = [
 #latex_use_parts = False
 
 # If true, show page references after internal links.
-#latex_show_pagerefs = False
+# latex_show_pagerefs = False
 
 # If true, show URL addresses after external links.
 #latex_show_urls = False
