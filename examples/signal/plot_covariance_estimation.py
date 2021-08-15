85c51b47992915f55884f78b30d6db3574e94388
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2021-08-15 16:12:24 +0200

    rewrite plots, reduce computation time

--- examples/signal/plot_covariance_estimation.py
+++ examples/signal/plot_covariance_estimation.py
@@ -3,7 +3,7 @@
 Estimate covariance with different time windows
 ===============================================================================
 
-Covariance estimators comparison for different EEG signal length and
+Covariance estimators comparison for different EEG signal lengths and their
 impact on classification [1]_.
 """
 # Author: Sylvain Chevallier
@@ -12,6 +12,7 @@ impact on classification [1]_.
 
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from matplotlib import pyplot as plt
 from numpy.linalg import eigh
 from mne import Epochs, pick_types, events_from_annotations
@@ -21,7 +22,6 @@ from mne.datasets import eegbci
 from pyriemann.estimation import Covariances
 from pyriemann.utils.distance import distance
 from pyriemann.classification import MDM, TSclassifier
-
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
@@ -34,49 +34,44 @@ from sklearn.pipeline import make_pipeline
 # Generate synthetic data, sampled from a distribution considered as the
 # groundtruth.
 
-Nt, Nc, Ns = 10, 5, 1000
-shape = (Nt, Nc, Nc)
-var = 2.0 + 0.1 * np.random.randn(Nt, Nc)
-A = 2 * np.random.rand(Nc, Nc) - 1
-A /= np.atleast_2d(np.sqrt(np.sum(A ** 2, 1))).T
-true_cov = np.empty(shape=shape)
-X = np.empty(shape=shape)
-for i in range(Nt):
+n_trials, n_channels, n_times = 10, 5, 1000
+var = 2.0 + 0.1 * np.random.randn(n_trials, n_channels)
+A = 2 * np.random.rand(n_channels, n_channels) - 1
+A /= np.linalg.norm(A, axis=1)[:, np.newaxis]
+true_cov = np.empty(shape=(n_trials, n_channels, n_channels))
+X = np.empty(shape=(n_trials, n_channels, n_times))
+for i in range(n_trials):
     true_cov[i] = A @ np.diag(var[i]) @ A.T
-    X[i] = np.random.multivariate_normal(np.array([0.0] * Nc), true_cov[i], size=Ns).T
+    X[i] = np.random.multivariate_normal(
+        np.array([0.0] * n_channels), true_cov[i], size=n_times
+    ).T
 
 ###############################################################################
-# Covariances() method offers several estimators: SCM, Ledoit-Wolf (LWF),
+# Covariances() object offers several estimators: SCM, Ledoit-Wolf (LWF),
 # Schaefer-Strimmer (SCH), oracle approximating shrunk covariance (OAS),
 # minimum covariance determinant (MCD) and others. We will compare the
 # distance of LWF, OAS and SCH estimators with the groundtruth, while
 # increasing number of samples.
 
 estimators = ["lwf", "oas", "sch"]
-w_len = np.linspace(10, Ns, 100, dtype=np.int)
-dist_true = np.zeros(shape=(len(estimators), len(w_len), Nt))
-for i, e in enumerate(estimators):
-    for j, t in enumerate(w_len):
-        cov_est = Covariances(estimator=e).transform(X[:, :, :t])
-        for k in range(Nt):
-            dist_true[i, j, k] = distance(cov_est[k], true_cov[k], metric="riemann")
+w_len = np.linspace(10, n_times, 20, dtype=np.int)
+dfd = list()
+for est in estimators:
+    for wl in w_len:
+        cov_est = Covariances(estimator=est).transform(X[:, :, :wl])
+        for k in range(n_trials):
+            dist = distance(cov_est[k], true_cov[k], metric="riemann")
+            dfd.append(dict(estimator=est, wlen=wl, dist=dist))
+dfd = pd.DataFrame(dfd)
 
 ###############################################################################
 
-fig, ax = plt.subplots(1)
-fig.set_size_inches(10, 8)
-for i, e in enumerate(estimators):
-    ax.plot(w_len, np.median(dist_true[i, :, :], axis=1), "-", label=e.upper())
-    ax.fill_between(
-        w_len,
-        np.percentile(dist_true[i, :, :], q=90, axis=1),
-        np.percentile(dist_true[i, :, :], q=10, axis=1),
-        alpha=0.5,
-    )
+fig, ax = plt.subplots(figsize=(6, 4))
+ax.set(xscale="log")
+sns.lineplot(data=dfd, x="wlen", y="dist", hue="estimator", ax=ax)
 ax.set_title("Distance to groundtruth covariance matrix")
 ax.set_xlabel("Number of samples")
-ax.set_ylabel(r"$\delta(\Sigma, \hat{\Sigma})$")
-_ = ax.legend()
+_ = ax.set_ylabel(r"$\delta(\Sigma, \hat{\Sigma})$")
 
 ###############################################################################
 # Choice of estimator for motor imagery data
@@ -99,7 +94,6 @@ picks = picks[::2]
 raw.filter(7.0, 35.0, method="iir", picks=picks, skip_by_annotation="edge")
 events, _ = events_from_annotations(raw, event_id=dict(T1=2, T2=3))
 event_ids = dict(hands=2, feet=3)
-# events = find_events(raw, shortest_event=0, stim_channel=None)
 
 ###############################################################################
 # Influence of shrinkage to estimate covariance
@@ -116,41 +110,34 @@ estimators = ["lwf", "oas", "scm", "sch"]
 tmin = -0.2
 w_len = np.linspace(0.2, 2, 10)
 n_trials = 45
-cond_nb = np.zeros(shape=(len(estimators), len(w_len), n_trials))
+dfc = list()
 
-for i, e in enumerate(estimators):
-    for j, t in enumerate(w_len):
-        epochs = Epochs(
-            raw,
-            events,
-            event_ids,
-            tmin,
-            tmin + t,
-            picks=picks,
-            preload=True,
-            verbose=False,
-        )
-        cov = Covariances(estimator=e).transform(epochs.get_data())
+for wl in w_len:
+    epochs = Epochs(
+        raw,
+        events,
+        event_ids,
+        tmin,
+        tmin + wl,
+        picks=picks,
+        preload=True,
+        verbose=False,
+    )
+    for est in estimators:
+        cov = Covariances(estimator=est).transform(epochs.get_data())
         for k in range(len(cov)):
-            w, _ = eigh(cov[k, :, :])
-            cond_nb[i, j, k] = w[-1] / w[0]
+            ev, _ = eigh(cov[k, :, :])
+            dfc.append(dict(estimator=est, wlen=wl, cond=ev[-1] / ev[0]))
+dfc = pd.DataFrame(dfc)
 
 ###############################################################################
 
-fig, ax = plt.subplots(1)
-fig.set_size_inches(10, 8)
-for i, e in enumerate(estimators):
-    ax.semilogy(w_len, np.median(cond_nb[i, :, :], axis=1), "-o", label=e.upper())
-    ax.fill_between(
-        w_len,
-        np.percentile(cond_nb[i, :, :], q=90, axis=1),
-        np.percentile(cond_nb[i, :, :], q=10, axis=1),
-        alpha=0.2,
-    )
+fig, ax = plt.subplots(figsize=(6, 4))
+ax.set(yscale="log")
+sns.lineplot(data=dfc, x="wlen", y="cond", hue="estimator", ax=ax)  # style="estimator",
 ax.set_title("Condition number of estimated covariance matrices")
 ax.set_xlabel("Epoch length (s)")
-ax.set_ylabel(r"$\lambda_{\min}$/$\lambda_{\max}$")
-ax.legend()
+_ = ax.set_ylabel(r"$\lambda_{\min}$/$\lambda_{\max}$")
 
 ###############################################################################
 # Picking a good estimator for classification
@@ -159,53 +146,56 @@ ax.legend()
 # especially when the covariances are estimated on short time windows.
 
 estimators = ["lwf", "oas", "scm", "sch"]
-tmin = -0.2
-w_len = np.linspace(0.1, 1, 10)
-n_trials, n_splits = 45, 10
-acc = np.zeros(shape=(len(estimators), len(w_len), n_splits))
+tmin = 0.0
+w_len = np.linspace(0.2, 2.0, 5)
+n_trials, n_splits = 45, 3
+# balanced accuracy: tmin = -0.2; w_len = np.linspace(0.2, 2.2, 5);  n_trials, n_splits = 45, 5
+# bl2 = tmin = -0.; w_len = np.linspace(0.2, 2., 5); n_trials, n_splits = 45, 5
+dfa = list()
 
 cv = StratifiedKFold(n_splits=n_splits, shuffle=True)
-for i, e in enumerate(estimators):
-    clf = make_pipeline(Covariances(estimator=e), TSclassifier(metric="riemann"))
-    for j, t in enumerate(w_len):
-        epochs = Epochs(
-            raw,
-            events,
-            event_ids,
-            tmin,
-            tmin + t,
-            proj=True,
-            picks=picks,
-            baseline=None,
-            preload=True,
-            verbose=False,
-        )
-        X, y = epochs.get_data(), list()
-        ev = epochs.events
-        for k in range(len(ev)):
-            if ev[k, -1] == 2:
-                y.append(0)
-            elif ev[k, -1] == 3:
-                y.append(1)
-        y = np.array(y)
+for wl in w_len:
+    epochs = Epochs(
+        raw,
+        events,
+        event_ids,
+        tmin,
+        tmin + wl,
+        proj=True,
+        picks=picks,
+        preload=True,
+        baseline=None,
+        verbose=False,
+    )
+    X = epochs.get_data()
+    y = np.array([0 if ev == 2 else 1 for ev in epochs.events[:, -1]])
+    for est in estimators:
+        clf = make_pipeline(Covariances(estimator=est), MDM())
         try:
-            acc[i, j, :] = cross_val_score(clf, X, y, cv=cv, scoring="roc_auc")
+            score = cross_val_score(clf, X, y, cv=cv, scoring="balanced_accuracy")
+            dfa += [dict(estimator=est, wlen=wl, accuracy=sc) for sc in score]
         except ValueError:
             print(f"{e}: {t} is not sufficent to estimate a PSD matrix")
-            acc[i, j, :] = [np.nan] * n_splits
+            dfa += [dict(estimator=est, wlen=wl, accuracy=np.nan)] * n_splits
+dfa = pd.DataFrame(dfa)
 
 ###############################################################################
 
-fig, ax = plt.subplots(1)
-fig.set_size_inches(10, 8)
-for i, e in enumerate(estimators):
-    ax.plot(w_len, np.mean(acc[i, :, :], axis=1), "-o", label=e.upper())
-#    ax.fill_between(w_len, np.percentile(acc[i, :, :], q=75, axis=1),
-#                    np.percentile(acc[i, :, :], q=25, axis=1), alpha=0.2)
-ax.set_title("Accuracy for different estimator and epoch length")
+fig, ax = plt.subplots(figsize=(6, 4))
+sns.lineplot(
+    data=dfa,
+    x="wlen",
+    y="accuracy",
+    hue="estimator",
+    style="estimator",
+    ax=ax,
+    ci=None,
+    markers=True,
+    dashes=False,
+)
+ax.set_title("Accuracy for different estimators and epoch lengths")
 ax.set_xlabel("Epoch length (s)")
-ax.set_ylabel(r"Accuracy (\%)")
-ax.legend()
+_ = ax.set_ylabel(r"Accuracy (\%)")
 
 ###############################################################################
 # References
