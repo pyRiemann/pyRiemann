4fc3d34de7568e4d9eb7338a1f66dc4224381d92
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2021-09-02 18:19:36 +0200

    correct flake8

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy.testing import assert_array_equal, assert_array_almost_equal
+from numpy.testing import assert_array_almost_equal
 from pyriemann.spatialfilters import Whitening
 import pytest
 
