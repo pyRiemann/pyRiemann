ee450a552fd1b569e6d200809965660fdf02b177
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2021-09-02 18:19:36 +0200

    correct flake8

--- tests/test_utils_covariance.py
+++ tests/test_utils_covariance.py
@@ -1,4 +1,4 @@
-from numpy.testing import assert_array_almost_equal, assert_array_equal
+from numpy.testing import assert_array_almost_equal
 import numpy as np
 from scipy.signal import welch, csd, coherence as coherence_sp
 import pytest
