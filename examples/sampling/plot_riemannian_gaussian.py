1409a4b4612f0f6245406e8ff19c5fde9916f323
Pedro Rodrigues <pedro.rodrigues@melix.org>
2021-09-14 10:04:28 +0200

    correcting pep8 issues

--- examples/sampling/plot_riemannian_gaussian.py
+++ examples/sampling/plot_riemannian_gaussian.py
@@ -3,7 +3,7 @@
 Sample from the Riemannian Gaussian distribution in the SPD manifold
 =====================================================================
 
-Spectral embedding of samples from the Riemannian Gaussian distribution 
+Spectral embedding of samples from the Riemannian Gaussian distribution
 with different centerings and dispersions
 
 """
@@ -24,27 +24,27 @@ print(__doc__)
 
 ###############################################################################
 # Set parameters for sampling from the Riemannian Gaussian distribution
-n_samples = 100 # how many samples to generate
-n_dim = 4 # number of dimensions of the SPD matrices
-sigma = 1.0 # dispersion of the Gaussian distribution
-epsilon = 4.0 # parameter for controlling the distance between centers
+n_samples = 100  # how many samples to generate
+n_dim = 4  # number of dimensions of the SPD matrices
+sigma = 1.0  # dispersion of the Gaussian distribution
+epsilon = 4.0  # parameter for controlling the distance between centers
 
 # Generate the samples on three different conditions
-Ybar = generate_random_spd_matrix(n_dim) # random reference point
-samples_1 = sample_gaussian_spd(n_samples=n_samples, 
-                                Ybar=Ybar, 
+Ybar = generate_random_spd_matrix(n_dim)  # random reference point
+samples_1 = sample_gaussian_spd(n_samples=n_samples,
+                                Ybar=Ybar,
                                 sigma=sigma,
-                                show_progress_bar=True)                                
-samples_2 = sample_gaussian_spd(n_samples=n_samples, 
-                                Ybar=Ybar, 
-                                sigma=sigma/2, 
                                 show_progress_bar=True)
-samples_3 = sample_gaussian_spd(n_samples=n_samples, 
-                                Ybar=epsilon*Ybar, 
+samples_2 = sample_gaussian_spd(n_samples=n_samples,
+                                Ybar=Ybar,
+                                sigma=sigma/2,
+                                show_progress_bar=True)
+samples_3 = sample_gaussian_spd(n_samples=n_samples,
+                                Ybar=epsilon*Ybar,
                                 sigma=sigma,
                                 show_progress_bar=True)
 
-# Stack all of the samples into one data array for the embedding                                
+# Stack all of the samples into one data array for the embedding
 samples = np.concatenate([samples_1, samples_2, samples_3])
 labels = np.array(n_samples*[1] + n_samples*[2] + n_samples*[3])
 
@@ -56,11 +56,11 @@ embd = lapl.fit_transform(X=samples)
 ###############################################################################
 # Plot the results
 
-fig, ax = plt.subplots(figsize=(8,6))
+fig, ax = plt.subplots(figsize=(8, 6))
 
-colors = {1:'C0', 2:'C1', 3:'C2'}
+colors = {1: 'C0', 2: 'C1', 3: 'C2'}
 for i in range(len(samples)):
-    ax.scatter(embd[i,0], embd[i,1], c=colors[labels[i]], s=50)
+    ax.scatter(embd[i, 0], embd[i, 1], c=colors[labels[i]], s=50)
 ax.scatter([], [], c='C0', s=50, label=r'$\varepsilon = 1.00, \sigma = 1.00$')
 ax.scatter([], [], c='C1', s=50, label=r'$\varepsilon = 1.00, \sigma = 0.50$')
 ax.scatter([], [], c='C2', s=50, label=r'$\varepsilon = 4.00, \sigma = 1.00$')
@@ -68,7 +68,7 @@ ax.set_xticks([-1, -0.5, 0, 0.5, 1.0])
 ax.set_xticklabels([-1, -0.5, 0, 0.5, 1.0], fontsize=12)
 ax.set_yticks([-1, -0.5, 0, 0.5, 1.0])
 ax.set_yticklabels([-1, -0.5, 0, 0.5, 1.0], fontsize=12)
-ax.set_title(r'Spectral embedding of data points (fixed $n_{dim} = 4$)', 
+ax.set_title(r'Spectral embedding of data points (fixed $n_{dim} = 4$)',
              fontsize=14)
 ax.set_xlabel(r'$\phi_1$', fontsize=14)
 ax.set_ylabel(r'$\phi_2$', fontsize=14)
