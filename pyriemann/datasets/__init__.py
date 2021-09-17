52a33918bc4e45ca48e92664fd9a3a9bc7553bae
Pedro Rodrigues <pedro.rodrigues@melix.org>
2021-09-17 13:54:48 +0200

    more updates

---
+++ pyriemann/datasets/__init__.py
@@ -0,0 +1,7 @@
+from .sampling import sample_gaussian_spd, generate_random_spd_matrix
+from .simulated import make_gaussian_blobs
+
+
+__all__ = ["sample_gaussian_spd",
+           "generate_random_spd_matrix",
+           "make_gaussian_blobs"]
