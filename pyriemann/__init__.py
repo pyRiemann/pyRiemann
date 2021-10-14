8f0ba47d13bb6697f08ef80e04f6dd490f6cb4f0
qbarthelemy <q.barthelemy@gmail.com>
2021-10-14 09:01:09 +0200

    re-add init file

---
+++ pyriemann/__init__.py
@@ -0,0 +1,24 @@
+from ._version import __version__
+from . import classification
+from . import tangentspace
+from . import channelselection
+from . import estimation
+from . import spatialfilters
+from . import clustering
+from . import stats
+from . import embedding
+from . import preprocessing
+
+
+__all__ = [
+    '__version__',
+    'classification',
+    'tangentspace',
+    'channelselection',
+    'estimation',
+    'spatialfilters',
+    'clustering',
+    'stats',
+    'embedding',
+    'preprocessing',
+]
