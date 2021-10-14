062d860ba179e7704b1da7882c70551b210a01a0
qbarthelemy <q.barthelemy@gmail.com>
2021-10-14 22:27:40 +0200

    re-add tests for raise errors

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -1,8 +1,10 @@
+import pytest
+import numpy as np
+
 from pyriemann.datasets.sampling import (sample_gaussian_spd,
                                          generate_random_spd_matrix)
 from pyriemann.utils.distance import distance_riemann
 from pyriemann.utils.test import is_positive_definite
-import numpy as np
 
 
 def test_sample_gaussian_spd():
@@ -31,3 +33,20 @@ def test_sigma_gaussian_spd():
     avg_d1 = np.mean([distance_riemann(X1_i, mean) for X1_i in X1])
     avg_d2 = np.mean([distance_riemann(X2_i, mean) for X2_i in X2])
     assert avg_d1 < avg_d2
+
+
+def test_functions_error():
+    n_matrices, n_dim = 10, 16
+    mean, sigma = np.eye(n_dim), 2.
+    with pytest.raises(ValueError):  # mean is not a matrix
+        sample_gaussian_spd(n_matrices, np.ones(n_dim), sigma)
+    with pytest.raises(ValueError):  # sigma is not a scalar
+        sample_gaussian_spd(n_matrices, mean, np.ones(n_dim))
+    with pytest.raises(ValueError):  # n_matrices is negative
+        sample_gaussian_spd(-n_matrices, mean, sigma)
+    with pytest.raises(ValueError):  # n_matrices is not an integer
+        sample_gaussian_spd(4.2, mean, sigma)
+    with pytest.raises(ValueError):  # n_dim is not an integer
+        generate_random_spd_matrix(1.7)
+    with pytest.raises(ValueError):  # n_dim is negative
+        generate_random_spd_matrix(-n_dim)
