6b05dfeddac4e420085e2d63b47762c3a9c0c726
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2021-10-20 02:15:36 +0200

    use rndstate fixture

--- tests/test_utils_test.py
+++ tests/test_utils_test.py
@@ -15,27 +15,24 @@ def test_is_square():
     assert not is_square(np.ones((n_channels, n_channels + 1)))
 
 
-def test_is_sym():
+def test_is_sym(rndstate):
     assert is_sym(np.eye(n_channels))
-    rs = np.random.RandomState(1234)
-    A = rs.randn(n_channels, n_channels)
+    A = rndstate.randn(n_channels, n_channels)
     assert is_sym(A + A.T)
     assert not is_sym(-A + A.T)
     assert not is_sym(np.ones((n_channels, n_channels + 1)))
 
 
-def test_is_skew_sym():
-    rs = np.random.RandomState(1234)
-    A = rs.randn(n_channels, n_channels)
+def test_is_skew_sym(rndstate):
+    A = rndstate.randn(n_channels, n_channels)
     assert is_skew_sym(A - A.T)
     assert not is_skew_sym(np.eye(n_channels))
     assert not is_skew_sym(A + A.T)
     assert not is_skew_sym(np.ones((n_channels, n_channels + 1)))
 
 
-def test_is_real():
-    rs = np.random.RandomState(1234)
-    A = rs.randn(n_channels, n_channels + 2)
+def test_is_real(rndstate):
+    A = rndstate.randn(n_channels, n_channels + 2)
     B = np.zeros((n_channels, n_channels + 2), dtype=complex)
     B.real = A
     assert is_real(B)
@@ -43,46 +40,41 @@ def test_is_real():
     assert not is_real(B)
 
 
-def test_is_hermitian():
-    rs = np.random.RandomState(1234)
-    A = rs.randn(n_channels, n_channels)
+def test_is_hermitian(rndstate):
+    A = rndstate.randn(n_channels, n_channels)
     B = np.zeros((n_channels, n_channels), dtype=complex)
     B.real, B.imag = A + A.T, A - A.T
     assert is_hermitian(B)
     assert not is_hermitian(np.ones((n_channels, n_channels + 1)))
 
 
-def test_is_pos_def():
+def test_is_pos_def(rndstate):
     assert is_pos_def(np.eye(n_channels))
-    rs = np.random.RandomState(1234)
-    A = rs.randn(n_channels, 100)
+    A = rndstate.randn(n_channels, 100)
     assert is_pos_def(A @ A.T + 0.001 * np.eye(n_channels))
     assert not is_pos_def(-A @ A.T)
     assert not is_pos_def(np.ones((n_channels, n_channels + 1)))
 
 
-def test_is_pos_semi_def():
+def test_is_pos_semi_def(rndstate):
     assert is_pos_semi_def(np.eye(n_channels))
-    rs = np.random.RandomState(1234)
-    A = rs.randn(n_channels, 100)
+    A = rndstate.randn(n_channels, 100)
     assert is_pos_semi_def(A @ A.T)
     assert not is_pos_semi_def(-A @ A.T)
     assert not is_pos_semi_def(np.ones((n_channels, n_channels + 1)))
 
 
-def test_is_sym_pos_def():
+def test_is_sym_pos_def(rndstate):
     assert is_sym_pos_def(np.eye(n_channels))
-    rs = np.random.RandomState(1234)
-    A = rs.randn(n_channels, 100)
+    A = rndstate.randn(n_channels, 100)
     assert is_sym_pos_def(A @ A.T + 0.001 * np.eye(n_channels))
     assert not is_sym_pos_def(-A @ A.T)
     assert not is_sym_pos_def(np.ones((n_channels, n_channels + 1)))
 
 
-def test_is_sym_pos_semi_def():
+def test_is_sym_pos_semi_def(rndstate):
     assert is_sym_pos_semi_def(np.eye(n_channels))
-    rs = np.random.RandomState(1234)
-    A = rs.randn(n_channels, 100)
+    A = rndstate.randn(n_channels, 100)
     assert is_sym_pos_semi_def(A @ A.T)
     assert not is_sym_pos_semi_def(-A @ A.T)
     assert not is_sym_pos_semi_def(np.ones((n_channels, n_channels + 1)))
