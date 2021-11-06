44a32c3b67da28e3e8578cfcdce8c30fbb0fe37d
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-11-06 12:10:37 +0100

    fix seeds to avoid eisenbugs

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -11,7 +11,7 @@ def test_sample_gaussian_spd():
     """Test Riemannian Gaussian sampling."""
     n_matrices, n_dim, sigma = 50, 16, 2.
     mean = np.eye(n_dim)
-    X = sample_gaussian_spd(n_matrices, mean, sigma, random_state=None)
+    X = sample_gaussian_spd(n_matrices, mean, sigma, random_state=42)
     assert X.shape == (n_matrices, n_dim, n_dim)  # X shape mismatch
     assert is_spd(X)  # X is an array of SPD matrices
 
@@ -28,8 +28,8 @@ def test_sigma_gaussian_spd():
     """Test sigma parameter from Riemannian Gaussian sampling."""
     n_matrices, n_dim, sig_1, sig_2 = 50, 8, 1., 2.
     mean = np.eye(n_dim)
-    X1 = sample_gaussian_spd(n_matrices, mean, sig_1, random_state=None)
-    X2 = sample_gaussian_spd(n_matrices, mean, sig_2, random_state=None)
+    X1 = sample_gaussian_spd(n_matrices, mean, sig_1, random_state=42)
+    X2 = sample_gaussian_spd(n_matrices, mean, sig_2, random_state=66)
     avg_d1 = np.mean([distance_riemann(X1_i, mean) for X1_i in X1])
     avg_d2 = np.mean([distance_riemann(X2_i, mean) for X2_i in X2])
     assert avg_d1 < avg_d2
