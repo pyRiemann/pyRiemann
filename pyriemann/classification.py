171785cd74ed64070822804eec05a6db40fe8e20
Emmanuel K. Kalunga <emmanuelkalunga.k@gmail.com>
2021-12-11 06:41:43 +0200

    Fix flake8 errors

--- pyriemann/classification.py
+++ pyriemann/classification.py
@@ -523,7 +523,6 @@ class KNearestNeighbor(MDM):
         out, _ = stats.mode(neighbors_classes[:, 0:self.n_neighbors], axis=1)
         return out.ravel()
 
-<<<<<<< HEAD
     def predict_proba(self, X):
         """Predict proba using softmax.
 
@@ -717,16 +716,17 @@ class SVC(sklearnSVC):
         else:
             raise TypeError(f"kernel must be 'precomputed' or callable, is "
                             f"{self.kernel}.")
-=======
+
+
 class MDWM (MDM):
     """Classification by Minimum Distance to Weighted Mean.
 
-    Classification by nearest centroid. For each of the given classes, a 
+    Classification by nearest centroid. For each of the given classes, a
     centroid is estimated, according to the chosen metric, as a weighted mean
-    of point (i.e. covariance matrices) from the source domain, combined with 
-    the class centroid of the target domain. 
-    For classification, a given new point is attibuted to the class whose centroid is 
-    the nearest according to the chosen metric.
+    of point (i.e. covariance matrices) from the source domain, combined with
+    the class centroid of the target domain.
+    For classification, a given new point is attibuted to the class whose
+    centroid is the nearest according to the chosen metric.
 
     Parameters
     ----------
@@ -739,7 +739,7 @@ class MDWM (MDM):
         the mean in order to boost the computional speed and 'riemann' for the
         distance in order to keep the good sensitivity for the classification.
     L : float, (default: 0)
-        Transfer coefficient. This parameter controls the trade-off between 
+        Transfer coefficient. This parameter controls the trade-off between
         source and target data.
     n_jobs : int, (default: 1)
         The number of jobs to use for the computation. This works by computing
@@ -765,18 +765,17 @@ class MDWM (MDM):
 
     References
     ----------
-    [1] E. Kalunga, S. Chevallier and Q. Barthélemy, "Transfer learning for 
-    SSVEP-based BCI using Riemannian similarities between users", in 26th 
+    [1] E. Kalunga, S. Chevallier and Q. Barthélemy, "Transfer learning for
+    SSVEP-based BCI using Riemannian similarities between users", in 26th
     European Signal Processing Conference (EUSIPCO), pp. 1685-1689. IEEE, 2018.
 
-    [2] S. Khazem, S. Chevallier, Q. Barthélemy, K. Haroun and C. Noûs, 
-    "Minimizing Subject-dependent Calibration for BCI with Riemannian Transfer 
-    Learning", in 10th International IEEE/EMBS Conference on Neural 
+    [2] S. Khazem, S. Chevallier, Q. Barthélemy, K. Haroun and C. Noûs,
+    "Minimizing Subject-dependent Calibration for BCI with Riemannian Transfer
+    Learning", in 10th International IEEE/EMBS Conference on Neural
     Engineering (NER), pp. 523-526. IEEE, 2021.
     """
 
-    def __init__(self,metric='riemann', L=0, n_jobs=1):
-        
+    def __init__(self, metric='riemann', L=0, n_jobs=1):
         """Init."""
         self.metric = metric
         self.n_jobs = n_jobs
@@ -797,7 +796,6 @@ class MDWM (MDM):
 
         else:
             raise TypeError('metric must be dict or str')
-
 
     def fit(self, X, y, X_source, y_source, sample_weight=None):
         """Fit (estimates) the centroids.
@@ -821,39 +819,42 @@ class MDWM (MDM):
         self : MDWM instance
             The MDWM instance.
         """
-        
+
         if set(y) != set(y_source):
             raise Exception(f"classes in source domain must match classes in target \
-                domain. Classes in source are {np.unique(y_source)} while classes \
-                    in target are {np.unique(y)}")
+                domain. Classes in source are {np.unique(y_source)} while \
+                    classes in target are {np.unique(y)}")
 
         self.classes_ = np.unique(y)
 
         if sample_weight is None:
             sample_weight = np.ones(X_source.shape[0])
-             
-        if self.n_jobs == 1:
-            self.target_means_ = [mean_covariance(X[y == l], 
-                                              metric=self.metric_mean)
-                                        for l in self.classes_]
 
-            self.domain_means_ = [mean_covariance(X_source[y_source == l], 
-                                                  metric=self.metric_mean,
-                                    sample_weight=sample_weight[y_source == l])
-                                        for l in self.classes_]
+        if self.n_jobs == 1:
+            self.target_means_ = [
+                mean_covariance(X[y == ll], metric=self.metric_mean)
+                for ll in self.classes_]
+
+            self.domain_means_ = [
+                mean_covariance(
+                    X_source[y_source == ll],
+                    metric=self.metric_mean,
+                    sample_weight=sample_weight[y_source == ll]
+                    )
+                for ll in self.classes_]
         else:
             self.target_means_ = Parallel(n_jobs=self.n_jobs)(
-                delayed(mean_covariance)(X[y == l], metric=self.metric_mean)
-                for l in self.classes_) 
+                delayed(mean_covariance)(X[y == ll], metric=self.metric_mean)
+                for ll in self.classes_)
             self.domain_means_ = Parallel(n_jobs=self.n_jobs)(
-                delayed(mean_covariance)(X_source[y_source == l],
-                                metric=self.metric_mean,
-                                sample_weight=sample_weight[y_source == l])
-                for l in self.classes_)
+                delayed(mean_covariance)(
+                    X_source[y_source == ll],
+                    metric=self.metric_mean,
+                    sample_weight=sample_weight[y_source == ll])
+                for ll in self.classes_)
 
-        self.class_center_ = [geodesic(self.target_means_[i], 
+        self.class_center_ = [geodesic(self.target_means_[i],
                                        self.domain_means_[i],
-                                       self.L, self.metric) 
-                for i, _ in enumerate(self.classes_)]
+                                       self.L, self.metric)
+                              for i, _ in enumerate(self.classes_)]
         return self
->>>>>>> 47bccb4 (add mdwm class)
