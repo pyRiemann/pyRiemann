b686377a2f4b738a2a8ba0cb5f1b1135d9e053b7
gabelstein <darkmash@web.de>
2021-12-16 23:40:22 +0100

    remove inner product, some cleanup

--- pyriemann/utils/kernel.py
+++ pyriemann/utils/kernel.py
@@ -1,27 +1,7 @@
 import numpy as np
 
 from .mean import mean_riemann
-from .base import sqrtm, invsqrtm, powm, logm, expm
-
-
-def inner_product(A, B, Cref):
-    """Inner product of symmetric matrices A and B on the tangent space of Cref.
-
-    Parameters
-    ----------
-    A : ndarray, shape (n_channels, n_channels)
-        First symmetric matrix to calculate inner product.
-    B : ndarray, shape (n_channels, n_channels)
-        Second symmetric matrix to calculate inner product.
-    Cref : ndarray, shape (n_channels, n_channels)
-        Reference point for the tangent space and inner product calculation.
-
-    Notes
-    -----
-    .. versionadded:: 0.2.8
-    """
-    Cref_inv = powm(Cref, -1)
-    return np.trace(Cref_inv@A@Cref_inv@B)
+from .base import invsqrtm, logm
 
 
 def kernel_riemann(X, Y=None, Cref=None):
@@ -60,25 +40,24 @@ def kernel_riemann(X, Y=None, Cref=None):
 
     n_matrices_X, n_channels, n_channels = X.shape
 
-    # tangent space projection
     X_ = np.matmul(G_invsq, np.matmul(X, G_invsq))
     X_ = np.array([logm(X_[index]) for index in range(n_matrices_X)])
 
-    if isinstance(Y, type(None)):
+    if isinstance(Y, type(None)) or np.array_equal(X, Y):
         Y_ = X_
 
     else:
         n_matrices_Y, n_channels, n_channels = Y.shape
 
-        Y = np.matmul(G_invsq, np.matmul(Y, G_invsq))
-        Y_ = np.array([logm(Y[index]) for index in range(n_matrices_Y)])
+        Y_ = np.matmul(G_invsq, np.matmul(Y, G_invsq))
+        Y_ = np.array([logm(Y_[index]) for index in range(n_matrices_Y)])
 
     # calculate scalar products
     # for i in range(n_matrices_X):
     #     for j in range(n_matrices_Y):
-    #         res[i][j] = np.trace(X_[i] @ Y_[j])
+    #         K[i][j] = np.trace(X_[i] @ Y_[j])
     # einsum does same as that just a looooooot faster
 
-    res = np.einsum('abc,dbc->ad', X_, Y_)
+    K = np.einsum('abc,dbc->ad', X_, Y_)
 
-    return res
+    return K
