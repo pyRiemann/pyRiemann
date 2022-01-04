7482ebeca8f13db58972ae04b94b0871747d7fb1
gabelstein <darkmash@web.de>
2022-01-05 00:53:17 +0100

    try spsd check in test_utils_kernel.py

--- tests/test_utils_kernel.py
+++ tests/test_utils_kernel.py
@@ -5,7 +5,7 @@ from pyriemann.utils.base import logm
 from numpy.testing import assert_array_equal, assert_array_almost_equal
 import numpy as np
 
-from pyriemann.utils.test import is_pos_def as is_spd
+from pyriemann.utils.test import is_pos_semi_def as is_spsd
 
 
 def test_riemann_kernel(rndstate, get_covmats):
@@ -13,11 +13,12 @@ def test_riemann_kernel(rndstate, get_covmats):
     n_matrices, n_channels = 5, 3
     cov = get_covmats(n_matrices, n_channels)
     K = kernel_riemann(cov, cov, np.eye(n_channels))
-    assert is_spd(K)
 
     log_cov = np.array([logm(c) for c in cov])
     tensor = np.tensordot(log_cov, log_cov.T, axes=1)
     K1 = np.trace(tensor, axis1=1, axis2=2)
+    assert is_spsd(K1)
+    assert is_spsd(K)
     assert_array_almost_equal(K, K1)
 
 
