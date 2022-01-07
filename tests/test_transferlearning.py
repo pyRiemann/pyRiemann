47bc3db5e83fc6f58eb3e7459a5c1a7119721c4e
Emmanuel K. Kalunga <emmanuelkalunga.k@gmail.com>
2022-01-07 15:53:11 +0200

    full test

--- tests/test_transferlearning.py
+++ tests/test_transferlearning.py
@@ -1,161 +1,175 @@
-# from conftest import get_distances, get_means, get_metrics
 import numpy as np
 from numpy.testing import assert_array_equal
 from pyriemann.transferlearning import MDWM
-# from pyriemann.classification import MDM, FgMDM, KNearestNeighbor, TSclassifier
-from pyriemann.estimation import Covariances
 import pytest
-from pytest import approx
-from sklearn.dummy import DummyClassifier
 
 
 # rclf = [MDM, FgMDM, KNearestNeighbor, TSclassifier]
 mat_transfer = [MDWM]
 
+
 @pytest.mark.parametrize("transfer", mat_transfer)
 class TransferTestCase:
-    @pytest.mark.parametrize("nn", [1, 2, 3, 4])
-    def test_transfer_with_target_sample(self, nn, transfer, get_covmats, get_labels):
-        n_classes = 4 
-        n_channels = 3
-        n_trials_source = n_classes * 3 
-        n_trials_target = n_classes * nn     # Issue: get_labels does not work for odd values of n
-        sample_weight=None
-        
+    def test_transfer_with_target_sample(self, transfer,
+                                         get_covmats, get_labels):
+        nn, n_classes, n_channels = 2, 2, 3
+        n_trials_source = n_classes * 3
+        n_trials_target = n_classes * nn
+        sample_weight = None
+
         covmats_source = get_covmats(n_trials_source, n_channels)
         labels_source = get_labels(n_trials_source, n_classes)
 
         covmats_target = get_covmats(n_trials_target, n_channels)
         labels_target = get_labels(n_trials_target, n_classes)
-        
+
         if transfer is MDWM:
-            self.trans_populate_classes(transfer, covmats_source, labels_source,
-                                        covmats_target, labels_target,
-                                        sample_weight)
+            self.trans_populate_classes(transfer, covmats_source,
+                                        labels_source, covmats_target,
+                                        labels_target, sample_weight)
             self.trans_predict(transfer, covmats_source, labels_source,
-                               covmats_target, labels_target,sample_weight)
-
-    def test_transfer_with_no_target_sample(self, transfer, get_covmats, get_labels):
-        n_classes = 4 
-        n_channels = 3
-        n_trials_source = n_classes * 3 
-        n_trials_target = 0     # Issue: get_labels does not work for odd values of n
-        sample_weight=None
-        
-        covmats_source = get_covmats(n_trials_source, n_channels)
-        labels_source = get_labels(n_trials_source, n_classes)
-
-        covmats_target = get_covmats(n_trials_target, n_channels)
-        labels_target = get_labels(n_trials_target, n_classes)
-
-        print(f"[DEBUG] len(labels_source): {len(labels_source)}")
-        print(f"[DEBUG] len(labels_target): {len(labels_target)}")
-        
-        if transfer is MDWM:
-            with pytest.raises(Exception):
-                self.trans_populate_classes(transfer, covmats_source, labels_source,
-                                            covmats_target, labels_target,
-                                            sample_weight)
-                # self.trans_predict(transfer, covmats_source, labels_source,
-                #                 covmats_target, labels_target,sample_weight)
+                               covmats_target, labels_target, sample_weight)
 
 
 class TestTransfer(TransferTestCase):
 
     def trans_populate_classes(self, transfer, covmats_source, labels_source,
-                             covmats_target, labels_target, sample_weight
-                            ):  
-        trans = transfer(Lambda=0.5)
+                               covmats_target, labels_target, sample_weight):
+        trans = transfer(transfer_coef=0.5)
         trans.fit(covmats_target, labels_target, covmats_source,
-                       labels_source, sample_weight)
+                  labels_source, sample_weight)
         assert_array_equal(trans.classes_, np.unique(labels_target))
-    
-    # def trans_predict(self, classif, covmats, labels):
+
     def trans_predict(self, transfer, covmats_source, labels_source,
-                             covmats_target, labels_target, sample_weight):
-        trans = transfer(Lambda=0.5)
+                      covmats_target, labels_target, sample_weight):
+        trans = transfer(transfer_coef=0.5)
         trans.fit(covmats_target, labels_target, covmats_source,
-                       labels_source, sample_weight)
+                  labels_source, sample_weight)
         predicted = trans.predict(covmats_target)
         assert predicted.shape == (len(labels_target),)
 
-
-# @pytest.mark.parametrize("classif", [MDM, FgMDM, TSclassifier])
-# @pytest.mark.parametrize("mean", ["faulty", 42])
-# @pytest.mark.parametrize("dist", ["not_real", 27])
-# def test_metric_dict_error(classif, mean, dist, get_covmats, get_labels):
-#     with pytest.raises((TypeError, KeyError)):
-#         n_trials, n_channels, n_classes = 6, 3, 2
-#         labels = get_labels(n_trials, n_classes)
-#         covmats = get_covmats(n_trials, n_channels)
-#         clf = classif(metric={"mean": mean, "distance": dist})
-#         clf.fit(covmats, labels).predict(covmats)
+    def trans_jobs(self, transfer, covmats_source, labels_source,
+                   covmats_target, labels_target, sample_weight):
+        trans = transfer(transfer_coef=0.5, n_jobs=2)
+        trans.fit(covmats_target, labels_target, covmats_source,
+                  labels_source, sample_weight)
+        trans.predict(covmats_target)
 
 
-# @pytest.mark.parametrize("classif", [MDM, FgMDM])
-# @pytest.mark.parametrize("mean", get_means())
-# @pytest.mark.parametrize("dist", get_distances())
-# def test_metric_dist(classif, mean, dist, get_covmats, get_labels):
-#     n_trials, n_channels, n_classes = 4, 3, 2
-#     labels = get_labels(n_trials, n_classes)
-#     covmats = get_covmats(n_trials, n_channels)
-#     clf = classif(metric={"mean": mean, "distance": dist})
-#     clf.fit(covmats, labels).predict(covmats)
+@pytest.mark.parametrize("transfer", [MDWM])
+@pytest.mark.parametrize("mean_keys", ["avg", "mean"])
+@pytest.mark.parametrize("dist_keys", ["dist", "distan"])
+def test_metric_dict_keys(transfer, mean_keys, dist_keys, get_covmats,
+                          get_labels):
+    with pytest.raises((KeyError)):
+        nn, n_classes, n_channels = 2, 2, 3
+        n_trials_source = n_classes * 3
+        n_trials_target = n_classes * nn
+        sample_weight = None
+
+        covmats_source = get_covmats(n_trials_source, n_channels)
+        labels_source = get_labels(n_trials_source, n_classes)
+
+        covmats_target = get_covmats(n_trials_target, n_channels)
+        labels_target = get_labels(n_trials_target, n_classes)
+
+        trans = transfer(transfer_coef=0.5, metric={mean_keys: 'riemann',
+                         dist_keys: 'riemann'})
+        trans.fit(covmats_target, labels_target, covmats_source,
+                  labels_source, sample_weight)
 
 
-# @pytest.mark.parametrize("classif", rclf)
-# @pytest.mark.parametrize("metric", [42, "faulty", {"foo": "bar"}])
-# def test_metric_wrong_keys(classif, metric, get_covmats, get_labels):
-#     with pytest.raises((TypeError, KeyError)):
-#         n_trials, n_channels, n_classes = 6, 3, 2
-#         labels = get_labels(n_trials, n_classes)
-#         covmats = get_covmats(n_trials, n_channels)
-#         clf = classif(metric=metric)
-#         clf.fit(covmats, labels).predict(covmats)
+@pytest.mark.parametrize("transfer", [MDWM])
+def test_metric_wrong_type(transfer, get_covmats, get_labels):
+    with pytest.raises((TypeError)):
+        nn, n_classes, n_channels = 2, 2, 3
+        n_trials_source = n_classes * 3
+        n_trials_target = n_classes * nn
+        sample_weight = None
+
+        covmats_source = get_covmats(n_trials_source, n_channels)
+        labels_source = get_labels(n_trials_source, n_classes)
+
+        covmats_target = get_covmats(n_trials_target, n_channels)
+        labels_target = get_labels(n_trials_target, n_classes)
+
+        trans = transfer(transfer_coef=0.5, metric=12)
+        trans.fit(covmats_target, labels_target, covmats_source,
+                  labels_source, sample_weight)
 
 
-# @pytest.mark.parametrize("classif", rclf)
-# @pytest.mark.parametrize("metric", get_metrics())
-# def test_metric_str(classif, metric, get_covmats, get_labels):
-#     n_trials, n_channels, n_classes = 6, 3, 2
-#     labels = get_labels(n_trials, n_classes)
-#     covmats = get_covmats(n_trials, n_channels)
-#     clf = classif(metric=metric)
-#     clf.fit(covmats, labels).predict(covmats)
+@pytest.mark.parametrize("transfer", [MDWM])
+@pytest.mark.parametrize("transfer_coef", [-0.1, 1.1])
+def test_transfer_coef_range(transfer, transfer_coef, get_covmats, get_labels):
+    with pytest.raises((ValueError)):
+        nn, n_classes, n_channels = 2, 2, 3
+        n_trials_source = n_classes * 3
+        n_trials_target = n_classes * nn
+        sample_weight = None
+
+        covmats_source = get_covmats(n_trials_source, n_channels)
+        labels_source = get_labels(n_trials_source, n_classes)
+
+        covmats_target = get_covmats(n_trials_target, n_channels)
+        labels_target = get_labels(n_trials_target, n_classes)
+
+        trans = transfer(transfer_coef=transfer_coef)
+        trans.fit(covmats_target, labels_target, covmats_source,
+                  labels_source, sample_weight)
 
 
-# @pytest.mark.parametrize("dist", ["not_real", 42])
-# def test_knn_dict_dist(dist, get_covmats, get_labels):
-#     with pytest.raises(KeyError):
-#         n_trials, n_channels, n_classes = 6, 3, 2
-#         labels = get_labels(n_trials, n_classes)
-#         covmats = get_covmats(n_trials, n_channels)
-#         clf = KNearestNeighbor(metric={"distance": dist})
-#         clf.fit(covmats, labels).predict(covmats)
+@pytest.mark.parametrize("transfer", [MDWM])
+def test_different_labels_error(transfer, get_covmats, get_labels):
+    with pytest.raises((ValueError)):
+        nn, n_classes, n_classes_source, n_channels = 2, 2, 4, 3
+        n_trials_source = n_classes_source * 3
+        n_trials_target = n_classes * nn
+        sample_weight = None
+
+        covmats_source = get_covmats(n_trials_source, n_channels)
+        labels_source = get_labels(n_trials_source, n_classes_source)
+
+        covmats_target = get_covmats(n_trials_target, n_channels)
+        labels_target = get_labels(n_trials_target, n_classes)
+
+        trans = transfer(transfer_coef=0.5)
+        trans.fit(covmats_target, labels_target, covmats_source,
+                  labels_source, sample_weight)
 
 
-# def test_1NN(get_covmats, get_labels):
-#     """Test KNearestNeighbor with K=1"""
-#     n_trials, n_channels, n_classes = 9, 3, 3
-#     covmats = get_covmats(n_trials, n_channels)
-#     labels = get_labels(n_trials, n_classes)
+@pytest.mark.parametrize("transfer", [MDWM])
+def test_different_labels_no_transfer(transfer, get_covmats, get_labels):
+    nn, n_classes, n_classes_source, n_channels = 2, 6, 4, 3
+    n_trials_source = n_classes_source * 3
+    n_trials_target = n_classes * nn
+    sample_weight = None
 
-#     knn = KNearestNeighbor(1, metric="riemann")
-#     knn.fit(covmats, labels)
-#     preds = knn.predict(covmats)
-#     assert_array_equal(labels, preds)
+    covmats_source = get_covmats(n_trials_source, n_channels)
+    labels_source = get_labels(n_trials_source, n_classes_source)
 
+    covmats_target = get_covmats(n_trials_target, n_channels)
+    labels_target = get_labels(n_trials_target, n_classes)
 
-# def test_TSclassifier_classifier(get_covmats, get_labels):
-#     """Test TS Classifier"""
-#     n_trials, n_channels, n_classes = 6, 3, 2
-#     covmats = get_covmats(n_trials, n_channels)
-#     labels = get_labels(n_trials, n_classes)
-#     clf = TSclassifier(clf=DummyClassifier())
-#     clf.fit(covmats, labels).predict(covmats)
+    trans = transfer(transfer_coef=0)
+    trans.fit(covmats_target, labels_target, covmats_source,
+              labels_source, sample_weight)
 
 
-# def test_TSclassifier_classifier_error():
-#     """Test TS if not Classifier"""
-#     with pytest.raises(TypeError):
-#         TSclassifier(clf=Covariances())
+@pytest.mark.parametrize("transfer", [MDWM])
+@pytest.mark.parametrize("excess", [-1, 1])
+def test_sample_weight_shape_error(transfer, excess, get_covmats, get_labels):
+    with pytest.raises((ValueError)):
+        nn, n_classes, n_classes_source, n_channels = 2, 4, 4, 3
+        n_trials_source = n_classes_source * 3
+        n_trials_target = n_classes * nn
+        sample_weight = np.ones(n_trials_source + excess)
+
+        covmats_source = get_covmats(n_trials_source, n_channels)
+        labels_source = get_labels(n_trials_source, n_classes_source)
+
+        covmats_target = get_covmats(n_trials_target, n_channels)
+        labels_target = get_labels(n_trials_target, n_classes)
+
+        trans = transfer(transfer_coef=0.5)
+        trans.fit(covmats_target, labels_target, covmats_source,
+                  labels_source, sample_weight)
