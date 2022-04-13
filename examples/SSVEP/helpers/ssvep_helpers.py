aeaf7c5e6c87a0b55114d14e3ffe214fe25c8f46
qbarthelemy <q.barthelemy@gmail.com>
2022-04-13 19:42:20 +0200

    improve helpers

--- examples/SSVEP/helpers/ssvep_helpers.py
+++ examples/SSVEP/helpers/ssvep_helpers.py
@@ -1,4 +1,4 @@
-""" Functions for SSVEP examples """
+""" Local functions for SSVEP examples """
 
 import os
 import numpy as np
@@ -24,8 +24,8 @@ def download_data(subject=1, session=1):
         Path to downloaded data
     """
     DATASET_URL = 'https://zenodo.org/record/2392979/files/'
-    url = '{:s}subject{:02d}_run{:d}_raw.fif'.format(DATASET_URL,
-                                                     subject, session + 1)
+    url = '{:s}subject{:02d}_run{:d}_raw.fif'.format(
+        DATASET_URL, subject, session + 1)
 
     archive_name = url.split('/')[-1]
     dataset_params = {
@@ -41,25 +41,30 @@ def download_data(subject=1, session=1):
     return os.path.join(data_path, archive_name)
 
 
-def bandpass_filter(signal, lowcut, highcut, method="iir"):
+def bandpass_filter(raw, l_freq, h_freq, method="iir", verbose=False):
     """ Band-pass filter a signal using MNE """
-    return signal.copy().filter(l_freq=lowcut, h_freq=highcut,
-                                method=method).get_data()
+    return raw.copy().filter(
+        l_freq=l_freq,
+        h_freq=h_freq,
+        method=method,
+        verbose=verbose
+    ).get_data()
 
 
 def extend_signal(raw, frequencies, freq_band):
     """ Extend a signal with filter bank using MNE """
-    ext_signal = np.vstack([
-        bandpass_filter(raw, lowcut=f-freq_band, highcut=f+freq_band)
+    raw_ext = np.vstack([
+        bandpass_filter(raw, l_freq=f - freq_band, h_freq=f + freq_band)
         for f in frequencies]
     )
 
     info = create_info(
         ch_names=sum(
-            list(map(lambda f: [ch+'-'+str(f)+'Hz' for ch in raw.ch_names],
+            list(map(lambda f: [ch + '-' + str(f) + 'Hz'
+                                for ch in raw.ch_names],
                      frequencies)), []),
         ch_types=['eeg'] * len(raw.ch_names) * len(frequencies),
         sfreq=int(raw.info['sfreq'])
     )
 
-    return RawArray(ext_signal, info)
+    return RawArray(raw_ext, info)
