29a2d3d3b1e13a9aa8bb30b0eb94dc52295877d1
qbarthelemy <q.barthelemy@gmail.com>
2022-05-02 19:03:09 +0200

    move example from 2d to 3d plots

--- examples/simulated/plot_classifier_comparison.py
+++ examples/simulated/plot_classifier_comparison.py
@@ -33,7 +33,7 @@ from pyriemann.classification import MDM, KNearestNeighbor
 ###############################################################################
 
 
-@partial(np.vectorize, excluded=['cov_11', 'clf'])
+@partial(np.vectorize, excluded=['clf'])
 def get_proba(cov_00, cov_01, cov_11, clf):
     cov = np.array([[cov_00, cov_01], [cov_01, cov_11]])
     with np.testing.suppress_warnings() as sup:
@@ -51,9 +51,11 @@ classifiers = [
     MDM(),
     KNearestNeighbor(n_neighbors=3)
 ]
+n_classifs = len(classifiers)
 
 rs = np.random.RandomState(2022)
 n_matrices, n_channels = 50, 2
+y = np.concatenate([np.zeros(n_matrices), np.ones(n_matrices)])
 
 datasets = [
     (
@@ -65,7 +67,7 @@ datasets = [
                 n_matrices, n_channels, rs, evals_mean=15, evals_std=1
             )
         ]),
-        np.concatenate([np.zeros(n_matrices), np.ones(n_matrices)])
+        y
     ),
     (
         np.concatenate([
@@ -76,7 +78,7 @@ datasets = [
                 n_matrices, n_channels, rs, evals_mean=12, evals_std=2
             )
         ]),
-        np.concatenate([np.zeros(n_matrices), np.ones(n_matrices)])
+        y
     ),
     make_gaussian_blobs(
         2*n_matrices, n_channels, random_state=rs, class_sep=3., class_disp=0.5
@@ -85,6 +87,10 @@ datasets = [
         2*n_matrices, n_channels, random_state=rs, class_sep=1., class_disp=0.7
     )
 ]
+n_datasets = len(datasets)
+
+cm = plt.cm.RdBu
+cm_bright = ListedColormap(["#FF0000", "#0000FF"])
 
 
 ###############################################################################
@@ -92,30 +98,25 @@ datasets = [
 figure = plt.figure(figsize=(8, 10))
 i = 1
 # iterate over datasets
-for ds_cnt, ds in enumerate(datasets):
+for ds_cnt, (X, y) in enumerate(datasets):
     # split dataset into training and test part
-    X, y = ds
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.4, random_state=42
     )
 
-    x_min, x_max = X[:, 0, 0].min() - 0.5, X[:, 0, 0].max() + 0.5
-    y_min, y_max = X[:, 0, 1].min() - 0.5, X[:, 0, 1].max() + 0.5
-    xx, yy = np.meshgrid(
-        np.arange(x_min, x_max, (x_max - x_min) / 50),
-        np.arange(y_min, y_max, (y_max - y_min) / 50)
-    )
+    x_min, x_max = X[:, 0, 0].min(), X[:, 0, 0].max()
+    y_min, y_max = X[:, 0, 1].min(), X[:, 0, 1].max()
+    z_min, z_max = X[:, 1, 1].min(), X[:, 1, 1].max()
 
     # just plot the dataset first
-    cm = plt.cm.RdBu
-    cm_bright = ListedColormap(["#FF0000", "#0000FF"])
-    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+    ax = plt.subplot(n_datasets, n_classifs + 1, i, projection='3d')
     if ds_cnt == 0:
         ax.set_title("Input data")
     # Plot the training points
     ax.scatter(
         X_train[:, 0, 0],
         X_train[:, 0, 1],
+        X_train[:, 1, 1],
         c=y_train,
         cmap=cm_bright,
         edgecolors="k"
@@ -124,35 +125,52 @@ for ds_cnt, ds in enumerate(datasets):
     ax.scatter(
         X_test[:, 0, 0],
         X_test[:, 0, 1],
+        X_test[:, 1, 1],
         c=y_test,
         cmap=cm_bright,
         alpha=0.6,
         edgecolors="k"
     )
-    ax.set_xlim(xx.min(), xx.max())
-    ax.set_ylim(yy.min(), yy.max())
-    ax.set_xticks(())
-    ax.set_yticks(())
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_zlim(z_min, z_max)
+    ax.set_xticklabels(())
+    ax.set_yticklabels(())
+    ax.set_zticklabels(())
     i += 1
+
+    rx = np.arange(x_min, x_max, (x_max - x_min) / 50)
+    ry = np.arange(y_min, y_max, (y_max - y_min) / 50)
+    rz = np.arange(z_min, z_max, (z_max - z_min) / 50)
 
     # iterate over classifiers
     for name, clf in zip(names, classifiers):
-        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+        ax = plt.subplot(n_datasets, n_classifs + 1, i, projection='3d')
         clf.fit(X_train, y_train)
         score = clf.score(X_test, y_test)
 
-        # Plot the decision boundary for the horizontal 2D plane going through
+        # Plot the decision boundaries for horizontal 2D planes going through
         # the mean value of the third coordinates
-        Z = get_proba(xx, yy, cov_11=X[:, 1, 1].mean(), clf=clf)
-        Z = np.ma.masked_where(~np.isfinite(Z), Z)
+        xx, yy = np.meshgrid(rx, ry)
+        zz = get_proba(xx, yy, X[:, 1, 1].mean() * np.ones_like(xx), clf=clf)
+        zz = np.ma.masked_where(~np.isfinite(zz), zz)
+        ax.contourf(xx, yy, zz, zdir='z', offset=z_min, cmap=cm, alpha=0.5)
 
-        # Put the result into a color plot
-        ax.contourf(xx, yy, Z, cmap=cm, alpha=0.8)
+        xx, zz = np.meshgrid(rx, rz)
+        yy = get_proba(xx, X[:, 0, 1].mean() * np.ones_like(xx), zz, clf=clf)
+        yy = np.ma.masked_where(~np.isfinite(yy), yy)
+        ax.contourf(xx, yy, zz, zdir='y', offset=y_max, cmap=cm, alpha=0.5)
+
+        yy, zz = np.meshgrid(ry, rz)
+        xx = get_proba(X[:, 0, 0].mean() * np.ones_like(yy), yy, zz, clf=clf)
+        xx = np.ma.masked_where(~np.isfinite(xx), xx)
+        ax.contourf(xx, yy, zz, zdir='x', offset=x_min, cmap=cm, alpha=0.5)
 
         # Plot the training points
         ax.scatter(
             X_train[:, 0, 0],
             X_train[:, 0, 1],
+            X_train[:, 1, 1],
             c=y_train,
             cmap=cm_bright,
             edgecolors="k"
@@ -161,28 +179,33 @@ for ds_cnt, ds in enumerate(datasets):
         ax.scatter(
             X_test[:, 0, 0],
             X_test[:, 0, 1],
+            X_test[:, 1, 1],
             c=y_test,
             cmap=cm_bright,
             edgecolors="k",
             alpha=0.6
         )
 
-        ax.set_xlim(xx.min(), xx.max())
-        ax.set_ylim(yy.min(), yy.max())
-        ax.set_xticks(())
-        ax.set_yticks(())
         if ds_cnt == 0:
             ax.set_title(name)
         ax.text(
-            xx.max() - 0.3,
-            yy.min() + 0.3,
+            1.3 * x_max,
+            y_min,
+            z_min,
             ("%.2f" % score).lstrip("0"),
             size=15,
             horizontalalignment="right",
+            verticalalignment ="bottom"
         )
+        ax.set_xlim(x_min, x_max)
+        ax.set_ylim(y_min, y_max)
+        ax.set_zlim(z_min, z_max)
+        ax.set_xticks(())
+        ax.set_yticks(())
+        ax.set_zticks(())
+
         i += 1
 
-plt.tight_layout()
 plt.show()
 
 
