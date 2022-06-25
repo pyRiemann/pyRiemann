6c9f546b942afc6bbb1f7a440d515ee768283150
qbarthelemy <q.barthelemy@gmail.com>
2022-06-25 15:24:37 +0200

    revert sort in ajd

--- pyriemann/utils/ajd.py
+++ pyriemann/utils/ajd.py
@@ -20,6 +20,91 @@ def _get_normalized_weight(sample_weight, data):
     return normalized_weight
 
 
+def rjd(X, eps=1e-8, n_iter_max=1000):
+    """Approximate joint diagonalization based on Jacobi angles.
+
+    This is a direct implementation of the AJD algorithm by Cardoso and
+    Souloumiac [1]_ used in JADE. The code is a translation of the Matlab code
+    provided in the author website.
+
+    Parameters
+    ----------
+    X : ndarray, shape (n_matrices, n_channels, n_channels)
+        Set of symmetric matrices to diagonalize.
+    eps : float, default=1e-8
+        Tolerance for stopping criterion.
+    n_iter_max : int, default=1000
+        The maximum number of iterations to reach convergence.
+
+    Returns
+    -------
+    V : ndarray, shape (n_channels, n_channels)
+        The diagonalizer, an orthogonal matrix.
+    D : ndarray, shape (n_matrices, n_channels, n_channels)
+        Set of quasi diagonal matrices.
+
+    Notes
+    -----
+    .. versionadded:: 0.2.4
+
+    See Also
+    --------
+    ajd_pham
+    uwedge
+
+    References
+    ----------
+    .. [1] J.-F. Cardoso and A. Souloumiac, "Jacobi angles for simultaneous
+        diagonalization", SIAM J Matrix Anal Appl, 1996
+    """
+
+    # reshape input matrix
+    A = np.concatenate(X, 0).T
+
+    # init variables
+    m, nm = A.shape  # n_channels, n_matrices_x_channels
+    V = np.eye(m)
+    encore = True
+    k = 0
+
+    while encore:
+        encore = False
+        k += 1
+        if k > n_iter_max:
+            break
+        for p in range(m - 1):
+            for q in range(p + 1, m):
+
+                Ip = np.arange(p, nm, m)
+                Iq = np.arange(q, nm, m)
+
+                # computation of Givens angle
+                g = np.array([A[p, Ip] - A[q, Iq], A[p, Iq] + A[q, Ip]])
+                gg = np.dot(g, g.T)
+                ton = gg[0, 0] - gg[1, 1]
+                toff = gg[0, 1] + gg[1, 0]
+                theta = 0.5 * np.arctan2(
+                    toff, ton + np.sqrt(ton * ton + toff * toff))
+                c = np.cos(theta)
+                s = np.sin(theta)
+                encore = encore | (np.abs(s) > eps)
+                if (np.abs(s) > eps):
+                    tmp = A[:, Ip].copy()
+                    A[:, Ip] = c * A[:, Ip] + s * A[:, Iq]
+                    A[:, Iq] = c * A[:, Iq] - s * tmp
+
+                    tmp = A[p, :].copy()
+                    A[p, :] = c * A[p, :] + s * A[q, :]
+                    A[q, :] = c * A[q, :] - s * tmp
+
+                    tmp = V[:, p].copy()
+                    V[:, p] = c * V[:, p] + s * V[:, q]
+                    V[:, q] = c * V[:, q] - s * tmp
+
+    D = np.reshape(A, (m, int(nm / m), m)).transpose(1, 0, 2)
+    return V, D
+
+
 def ajd_pham(X, eps=1e-6, n_iter_max=15, sample_weight=None):
     """Approximate joint diagonalization based on Pham's algorithm.
 
@@ -111,91 +196,6 @@ def ajd_pham(X, eps=1e-6, n_iter_max=15, sample_weight=None):
         if decr < epsilon:
             break
     D = np.reshape(A, (n_channels, -1, n_channels)).transpose(1, 0, 2)
-    return V, D
-
-
-def rjd(X, eps=1e-8, n_iter_max=1000):
-    """Approximate joint diagonalization based on Jacobi angles.
-
-    This is a direct implementation of the AJD algorithm by Cardoso and
-    Souloumiac [1]_ used in JADE. The code is a translation of the Matlab code
-    provided in the author website.
-
-    Parameters
-    ----------
-    X : ndarray, shape (n_matrices, n_channels, n_channels)
-        Set of symmetric matrices to diagonalize.
-    eps : float, default=1e-8
-        Tolerance for stopping criterion.
-    n_iter_max : int, default=1000
-        The maximum number of iterations to reach convergence.
-
-    Returns
-    -------
-    V : ndarray, shape (n_channels, n_channels)
-        The diagonalizer, an orthogonal matrix.
-    D : ndarray, shape (n_matrices, n_channels, n_channels)
-        Set of quasi diagonal matrices.
-
-    Notes
-    -----
-    .. versionadded:: 0.2.4
-
-    See Also
-    --------
-    ajd_pham
-    uwedge
-
-    References
-    ----------
-    .. [1] J.-F. Cardoso and A. Souloumiac, "Jacobi angles for simultaneous
-        diagonalization", SIAM J Matrix Anal Appl, 1996
-    """
-
-    # reshape input matrix
-    A = np.concatenate(X, 0).T
-
-    # init variables
-    m, nm = A.shape  # n_channels, n_matrices_x_channels
-    V = np.eye(m)
-    encore = True
-    k = 0
-
-    while encore:
-        encore = False
-        k += 1
-        if k > n_iter_max:
-            break
-        for p in range(m - 1):
-            for q in range(p + 1, m):
-
-                Ip = np.arange(p, nm, m)
-                Iq = np.arange(q, nm, m)
-
-                # computation of Givens angle
-                g = np.array([A[p, Ip] - A[q, Iq], A[p, Iq] + A[q, Ip]])
-                gg = np.dot(g, g.T)
-                ton = gg[0, 0] - gg[1, 1]
-                toff = gg[0, 1] + gg[1, 0]
-                theta = 0.5 * np.arctan2(
-                    toff, ton + np.sqrt(ton * ton + toff * toff))
-                c = np.cos(theta)
-                s = np.sin(theta)
-                encore = encore | (np.abs(s) > eps)
-                if (np.abs(s) > eps):
-                    tmp = A[:, Ip].copy()
-                    A[:, Ip] = c * A[:, Ip] + s * A[:, Iq]
-                    A[:, Iq] = c * A[:, Iq] - s * tmp
-
-                    tmp = A[p, :].copy()
-                    A[p, :] = c * A[p, :] + s * A[q, :]
-                    A[q, :] = c * A[q, :] - s * tmp
-
-                    tmp = V[:, p].copy()
-                    V[:, p] = c * V[:, p] + s * V[:, q]
-                    V[:, q] = c * V[:, q] - s * tmp
-
-    D = np.reshape(A, (m, int(nm / m), m)).transpose(1, 0, 2)
     return V, D
 
 
