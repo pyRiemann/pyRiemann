18d43e52019e70fbcba96bc436a7fc0b44dfd9da
qbarthelemy <q.barthelemy@gmail.com>
2022-09-05 23:20:48 +0200

    avoid to repeat inputs check in private functions at each iteration

--- pyriemann/utils/rotate.py
+++ pyriemann/utils/rotate.py
@@ -33,61 +33,27 @@ def _check_dimensions(M):
     return M
 
 
-def _loss(Q, X, Y, weights=None, metric='euclid'):
+def _loss(Q, X, Y, weights, metric='euclid'):
     """Loss function for estimating the rotation matrix in RPA."""
-
-    X = _check_dimensions(X)
-    Y = _check_dimensions(Y)
-
-    if len(X) != len(Y):
-        raise ValueError("The number of classes in each domain don't match")
-
-    if weights is None:
-        weights = np.ones(len(X)) / len(X)
 
     return weights @ distance(X, Q @ Y @ Q.T, metric=metric)
 
 
-def _grad_rie(Q, X, Y, weights=None):
-    """
-    Gradient of loss function using Riemannian distances between class means.
-    """
+def _grad(Q, X, Y, weights, metric='euclid'):
+    """Gradient of loss function between class means."""
 
-    X = _check_dimensions(X)
-    Y = _check_dimensions(Y)
+    if metric == 'euclid':
+        return np.einsum('a,abc->bc', weights, -4 * (X - Q @ Y @ Q.T) @ Q @ Y)
 
-    if len(X) != len(Y):
-        raise ValueError("The number of classes in each domain don't match")
+    elif metric == 'riemann':
+        M = np.linalg.inv(X) @ Q @ Y @ Q.T
+        eigvals, eigvecs = np.linalg.eig(M)
+        logeigvals = np.expand_dims(np.log(eigvals), -2)
+        logM = (eigvecs * logeigvals) @ np.linalg.inv(eigvecs)
+        return np.einsum('a,abc->bc', weights, 4 * logM @ Q)
 
-    if weights is None:
-        weights = np.ones(len(X)) / len(X)
-
-    M = np.linalg.inv(X) @ Q @ Y @ Q.T
-    eigvals, eigvecs = np.linalg.eig(M)
-    logeigvals = np.expand_dims(np.log(eigvals), -2)
-    logM = (eigvecs * logeigvals) @ np.linalg.inv(eigvecs)
-    G = np.einsum('a,abc->bc', weights, 4 * logM @ Q)
-
-    return G
-
-
-def _grad_euc(Q, X, Y, weights=None):
-    """
-    Gradient of loss function using Euclidean distances between class means.
-    """
-
-    X = _check_dimensions(X)
-    Y = _check_dimensions(Y)
-
-    if len(X) != len(Y):
-        raise ValueError("The number of classes in each domain don't match")
-
-    if weights is None:
-        weights = np.ones(len(X)) / len(X)
-
-    G = np.einsum('a,abc->bc', weights, -4 * (X - Q @ Y @ Q.T) @ Q @ Y)
-
-    return G
+    else:
+        raise ValueError("RPA supports only 'euclid' and 'riemann' metrics.")
 
 
 def _warm_start(X, Y, metric='euclid'):
@@ -117,14 +83,7 @@ def _warm_start(X, Y, metric='euclid'):
         Qstar = qX @ qY.T
         return Qstar
 
-    X = _check_dimensions(X)
-    Y = _check_dimensions(Y)
-
-    if len(X) != len(Y):
-        raise ValueError("The number of classes in each domain don't match")
-
     Q0_candidates = []
-
     for i in range(len(X)):
         Q0_candidates.append(_get_local_solution(X[i], Y[i]))
 
@@ -152,71 +111,70 @@ def get_rotation_matrix(M_source, M_target, weights=None, metric='euclid',
     Parameters
     ----------
     M_source : ndarray, shape (n_classes, n, n)
-        Set with the means of the n_classes from the source domain
+        Set with the means of the n_classes from the source domain.
     M_target : ndarray, shape (n_classes, n, n)
-        Set with the means of the n_classes from the target domain
+        Set with the means of the n_classes from the target domain.
     weights : None | array, shape (n_classes,), default=None
-        Set with the weights to assign for each class. If None, then give the
-        same weight for each class
+        Weights for each class. If None, then give the same weight for each
+        class.
     metric : str, default='euclid'
         Which type of distance to minimize between the class means, either
-        'euclid' for euclidean distance or 'riemann' for the AIRM-induced
-        distance between SPD matrices
+        'euclid' for Euclidean distance or 'riemann' for the affine-invariant
+        Riemannian distance between SPD matrices.
+    tol_step : float, default 1e-9
+        TODO
     maxiter : int, default=10_000
-        Maximum number of iterations in the optimization procedure
+        Maximum number of iterations in the optimization procedure.
     maxiter_linesearch : int, default=32
-        Maximum number of iterations in the line search procedure
+        Maximum number of iterations in the line search procedure.
 
     Returns
     -------
     Q : ndarray, shape (n, n)
         Orthogonal matrix that minimizes the distance between the class means
-        for the source and target domains
+        for the source and target domains.
 
     References
     ----------
     .. [1] P. Rodrigues et al. "Riemannian Procrustes Analysis: Transfer
            Learning for Brain-Computer Interfaces" (2018)
-
     .. [2] N. Boumal "An introduction to optimization on smooth manifolds"
     """
 
     M_source = _check_dimensions(M_source)
     M_target = _check_dimensions(M_target)
-
     if len(M_source) != len(M_target):
         raise ValueError("The number of classes in each domain don't match")
 
-    # decide which setup to use and the associated loss/grad functions
-    if metric == 'euclid':
-        _grad = _grad_euc
-    elif metric == 'riemann':
-        _grad = _grad_rie
+    if weights is None:
+        weights = np.ones(len(M_source)) / len(M_source)
 
     # initialize the solution with an educated guess
-    Qk_1 = _warm_start(M_target, M_source, metric=metric)
+    Q_1 = _warm_start(M_target, M_source, metric=metric)
 
     # loop over iterations
-    for iter in range(maxiter):
+    for _ in range(maxiter):
 
         # get the current value for the loss function
-        Fk_1 = _loss(Qk_1, M_target, M_source, weights, metric=metric)
+        F_1 = _loss(Q_1, M_target, M_source, weights, metric=metric)
 
         # get the direction of steepest descent
-        direction = _project(Qk_1, _grad(Qk_1, M_target, M_source, weights))
+        direction = _project(
+            Q_1, _grad(Q_1, M_target, M_source, weights, metric=metric)
+        )
 
         # backtracking line search
         alpha = 1.0
         tau = 0.50
         r = 1e-4
-        Qk = _retract(Qk_1, -alpha * direction)
-        Fk = _loss(Qk, M_target, M_source, metric=metric)
+        Q = _retract(Q_1, -alpha * direction)
+        F = _loss(Q, M_target, M_source, metric=metric)
         for iter_linesearch in range(maxiter_linesearch):
-            if Fk_1 - Fk > r * alpha * np.linalg.norm(direction)**2:
+            if F_1 - F > r * alpha * np.linalg.norm(direction)**2:
                 break
             alpha = tau * alpha
-            Qk = _retract(Qk_1, -alpha * direction)
-            Fk = _loss(Qk, M_target, M_source, weights, metric=metric)
+            Q = _retract(Q_1, -alpha * direction)
+            F = _loss(Q, M_target, M_source, weights, metric=metric)
 
         # test if the step size is small
         crit = np.linalg.norm(-alpha * direction)
@@ -224,10 +182,10 @@ def get_rotation_matrix(M_source, M_target, weights=None, metric='euclid',
             break
 
         # update variables for next iteration
-        Qk_1 = Qk
-        Fk_1 = Fk
+        Q_1 = Q
+        F_1 = F
 
     else:
         warnings.warn('Convergence not reached.')
 
-    return Qk
+    return Q
