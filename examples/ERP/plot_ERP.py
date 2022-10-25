101a59c811cc2179c93a7afa87209342763c3267
qbarthelemy <q.barthelemy@gmail.com>
2022-10-25 19:40:36 +0200

    fix build compilation error

--- examples/ERP/plot_ERP.py
+++ examples/ERP/plot_ERP.py
@@ -21,7 +21,7 @@ from pyriemann.utils.viz import plot_waveforms
 # -------------
 
 # Set filenames
-data_path = mne.datasets.sample.data_path()
+data_path = str(mne.datasets.sample.data_path())
 raw_fname = data_path + "/MEG/sample/sample_audvis_filt-0-40_raw.fif"
 event_fname = data_path + "/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif"
 
