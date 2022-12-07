Add the test of class_dis function, fix the class_dis formula, move the function to classification, and incorporate class_dis example into plot_toy_classification

examples/simulated/plot_class_distinctiveness.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 
 from pyriemann.embedding import SpectralEmbedding
 from pyriemann.datasets import make_gaussian_blobs
-from pyriemann.preprocessing import class_distinctiveness
+from pyriemann.classification import class_distinctiveness
 
 
 print(__doc__)
@@ -60,17 +60,17 @@ labels = [data0_y, data1_y, data2_y]
 ###############################################################################
 # Apply classDis for each dataset
 
-all_classDis = []
+all_class_dis = []
 for data_ind in range(len(datasets)):
-    classDis = class_distinctiveness(datasets[data_ind],
-                                     labels[data_ind], nume_denomi=False)
-    all_classDis.append(format(classDis, '.4f'))
+    class_dis = class_distinctiveness(datasets[data_ind],
+                                      labels[data_ind], return_num_denom=False)
+    all_class_dis.append(format(class_dis, '.4f'))
 
 ###############################################################################
 # Select the dataset with the highest classDis value
 
-max_classDis_ind = np.argmax(all_classDis)
-print('Best class-separated dataset is  dataset' + str(max_classDis_ind))
+max_classDis_ind = np.argmax(all_class_dis)
+print('Best class-separated dataset is dataset' + str(max_classDis_ind))
 
 ###############################################################################
 # Plot sample distribution of each dataset
@@ -104,3 +104,14 @@ for axi, step, title in zip(ax, steps, titles):
 ax[max_classDis_ind].set_title('best class-separated dataset\n'
                                + titles[max_classDis_ind], fontsize=14)
 plt.show()
+
+###############################################################################
+# References
+# ----------
+# .. [1] `Class-distinctiveness-based frequency band selection on the
+#    Riemannian manifold for oscillatory activity-based BCIs: preliminary
+#    results
+#    <https://hal.archives-ouvertes.fr/hal-03641137/>`_
+#    M. S. Yamamoto, F. Lotte, F. Yger, and S. Chevallier.
+#    44th Annual International Conference of the IEEE Engineering
+#    in Medicine & Biology Society (EMBC2022), 2022.

examples/simulated/plot_toy_classification.py
@@ -1,13 +1,15 @@
 """
 =====================================================================
-Illustrate classification accuracy versus class separability
+Illustrate classification accuracy and class distinctiveness value
+versus class separability
 =====================================================================
 
 Generate several datasets containing data points from two-classes. Each class
 is generated with a Riemannian Gaussian distribution centered at the class mean
 and with the same dispersion sigma. The distance between the class means is
 parametrized by Delta, which we make vary between zero and 5*sigma. We
-illustrate how the accuracy of the MDM classifier varies when Delta increases.
+illustrate how the accuracy of the MDM classifier and the value of the class
+distinctiveness vary when Delta increases.
 
 """
 # Authors: Pedro Rodrigues <pedro.rodrigues@melix.org>
@@ -16,10 +18,11 @@ illustrate how the accuracy of the MDM classifier varies when Delta increases.
 
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, StratifiedKFold
 
 from pyriemann.classification import MDM
 from pyriemann.datasets import make_gaussian_blobs
+from pyriemann.classification import class_distinctiveness
 
 
 print(__doc__)
@@ -37,6 +40,7 @@ random_state = 42  # ensure reproducibility
 ###############################################################################
 # Loop over different levels of separability between the classes
 scores_array = []
+class_dis_array = []
 deltas_array = np.linspace(0, 3*sigma, 10)
 
 for delta in deltas_array:
@@ -48,28 +52,66 @@ for delta in deltas_array:
                                random_state=random_state,
                                n_jobs=4)
 
+    # measure class distinctiveness of training data for each split
+    skf = StratifiedKFold(n_splits=5)
+    all_class_dis = []
+    for train_ind, test_ind in skf.split(X, y):
+        class_dis = class_distinctiveness(X[train_ind], y[train_ind],
+                                          return_num_denom=False)
+        all_class_dis.append(class_dis)
+    all_class_dis = np.array(all_class_dis)
+
+    # average class distinctiveness cross splits
+    mean_class_dis = np.mean(all_class_dis)
+    class_dis_array.append(mean_class_dis)
+
     # which classifier to consider
     clf = MDM()
 
     # get the classification score for this setup
     scores_array.append(
-        cross_val_score(clf, X, y, cv=5, scoring='roc_auc').mean())
+        cross_val_score(clf, X, y, cv=skf, scoring='roc_auc').mean())
 
 scores_array = np.array(scores_array)
+class_dis_array = np.array(class_dis_array)
 
 ###############################################################################
 # Plot the results
-fig, ax = plt.subplots(figsize=(7.5, 5.9))
-ax.plot(deltas_array, scores_array, lw=3.0, label=sigma)
-ax.set_xticks([0, 1, 2, 3])
-ax.set_xticklabels([0, 1, 2, 3], fontsize=12)
-ax.set_yticks([0.6, 0.7, 0.8, 0.9, 1.0])
-ax.set_yticklabels([0.6, 0.7, 0.8, 0.9, 1.0], fontsize=12)
-ax.set_xlabel(r'$\Delta/\sigma$', fontsize=14)
-ax.set_ylabel(r'score', fontsize=12)
-ax.set_title(r'Classification score Vs class separability ($n_{dim} = 4$)',
-             fontsize=12)
-ax.grid(True)
-ax.legend(loc='lower right', title=r'$\sigma$')
+fig, ax1 = plt.subplots(figsize=(7.5, 5.9))
+ax2 = ax1.twinx()
 
+ax1.plot(deltas_array, scores_array, lw=3.0, label=r'score ($\sigma$=1)')
+ax2.plot(deltas_array, class_dis_array, lw=3.0, color='g', label='class_dis')
+
+ax1.set_xticks([0, 1, 2, 3])
+ax1.set_xticklabels([0, 1, 2, 3], fontsize=12)
+
+ax1.set_yticks([0.6, 0.7, 0.8, 0.9, 1.0])
+ax1.set_yticklabels([0.6, 0.7, 0.8, 0.9, 1.0], fontsize=12)
+
+ax2.set_yticks([0.2, 0.5, 0.8, 1.1, 1.4, 1.7])
+ax2.set_yticklabels([0.2, 0.5, 0.8, 1.1, 1.4, 1.7], fontsize=12)
+
+ax1.set_xlabel(r'$\Delta/\sigma$', fontsize=14)
+ax1.set_ylabel(r'score', fontsize=12)
+ax2.set_ylabel(r'class distinctiveness', fontsize=12)
+ax1.set_title(r'Classification score and class distinctiveness '
+              r'value Vs class separability ($n_{dim} = 4$)',
+              fontsize=12)
+h1, l1 = ax1.get_legend_handles_labels()
+h2, l2 = ax2.get_legend_handles_labels()
+ax1.legend(h1+h2, l1+l2, loc='lower right')
+ax1.grid(True)
+ax2.grid(True)
 plt.show()
+
+###############################################################################
+# References
+# ----------
+# .. [1] `Class-distinctiveness-based frequency band selection on the
+#    Riemannian manifold for oscillatory activity-based BCIs: preliminary
+#    results
+#    <https://hal.archives-ouvertes.fr/hal-03641137/>`_
+#    M. S. Yamamoto, F. Lotte, F. Yger, and S. Chevallier.
+#    44th Annual International Conference of the IEEE Engineering
+#    in Medicine & Biology Society (EMBC2022), 2022.

pyriemann/classification.py
@@ -11,13 +11,12 @@ from sklearn.pipeline import make_pipeline
 from joblib import Parallel, delayed
 
 from .utils.kernel import kernel
-from .utils.mean import mean_covariance, mean_power
-from .utils.distance import distance
+from .utils.mean import mean_covariance, mean_power, mean_riemann
+from .utils.distance import distance, distance_riemann
 from .tangentspace import FGDA, TangentSpace
 
 
 def _check_metric(metric):
-
     if isinstance(metric, str):
         metric_mean = metric
         metric_dist = metric
@@ -141,7 +140,7 @@ class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
         else:
             dist = Parallel(n_jobs=self.n_jobs)(delayed(distance)(
                 X, self.covmeans_[m], self.metric_dist)
-                for m in range(n_centroids))
+                                                for m in range(n_centroids))
 
         dist = np.concatenate(dist, axis=1)
         return dist
@@ -195,7 +194,7 @@ class MDM(BaseEstimator, ClassifierMixin, TransformerMixin):
         prob : ndarray, shape (n_matrices, n_classes)
             Probabilities for each class.
         """
-        return softmax(-self._predict_distances(X)**2)
+        return softmax(-self._predict_distances(X) ** 2)
 
 
 class FgMDM(BaseEstimator, ClassifierMixin, TransformerMixin):
@@ -547,7 +546,7 @@ class KNearestNeighbor(MDM):
         idx = np.argsort(dist)
         dist_sorted = np.take_along_axis(dist, idx, axis=1)
         neighbors_classes = self.classmeans_[idx]
-        probas = softmax(-dist_sorted[:, 0:self.n_neighbors]**2)
+        probas = softmax(-dist_sorted[:, 0:self.n_neighbors] ** 2)
 
         prob = np.zeros((n_matrices, len(self.classes_)))
         for m in range(n_matrices):
@@ -819,7 +818,7 @@ class MeanField(BaseEstimator, ClassifierMixin, TransformerMixin):
         for ip, p in enumerate(self.power_list):
             for ill, ll in enumerate(labs_unique):
                 m[ip, ill] = distance(
-                    x, self.covmeans_[p][ll], metric=self.metric)**2
+                    x, self.covmeans_[p][ll], metric=self.metric) ** 2
 
         if self.method_label == 'sum_means':
             ipmin = np.argmin(np.sum(m, axis=1))
@@ -848,7 +847,7 @@ class MeanField(BaseEstimator, ClassifierMixin, TransformerMixin):
 
         pred = Parallel(n_jobs=self.n_jobs)(delayed(self._get_label)(
             x, labs_unique)
-            for x in X)
+                                            for x in X)
         return np.array(pred)
 
     def _predict_distances(self, X):
@@ -863,7 +862,7 @@ class MeanField(BaseEstimator, ClassifierMixin, TransformerMixin):
                     m[p].append(
                         distance(
                             x, self.covmeans_[p][ll], metric=self.metric
-                        )**2
+                        ) ** 2
                     )
             pmin = min(m.items(), key=lambda x: np.sum(x[1]))[0]
             dist.append(np.array(m[pmin]))
@@ -903,4 +902,105 @@ class MeanField(BaseEstimator, ClassifierMixin, TransformerMixin):
         prob : ndarray, shape (n_matrices, n_classes)
             Probabilities for each class.
         """
-        return softmax(-self._predict_distances(X)**2)
+        return softmax(-self._predict_distances(X) ** 2)
+
+
+def class_distinctiveness(X, y, return_num_denom=False):
+    r"""Measure class separability between classes on the manifold.
+
+    For two class problem, the class distinctiveness between class A
+    and B on the manifold is quantified as:
+
+    .. math::
+        classDis(A, B)=\frac{\delta_{R}
+                       \left(\bar{C}^{A}, \bar{C}^{B}\right)}
+                       {\frac{1}{2}
+                       \left( \sigma_{C^{A}} + \sigma_{C^{B}}
+                       \right)}
+
+    where :math:`\bar{C}^{K}` and :math:`\sigma_{C^{K}}` are
+    respectively the mean and the mean absolute deviation of the
+    covariance matrices from class K.
+
+    For more than two classes, it is quantified as:
+
+    .. math::
+        classDis\left(\left\{A_{i}\right\}\right) =
+        \frac{\sum_{i=1}^{N c} \delta_{R}\left(\bar{C}^{A_{i}},
+        \overline{\bar{C}}^{A}\right)}{\sum_{i=1}^{N c} \sigma_{C^{A_{i}}}}
+
+    where :math:`\overline{\bar{C}}^{A}` is the average of the mean covariance
+    matrices of all classes.
+
+    Parameters
+    ----------
+    X : ndarray, shape (n_matrices, n_channels, n_channels)
+        Set of SPD matrices.
+    y : ndarray, shape (n_matrices,)
+        Labels for each matrix.
+    return_num_denom : bool, default=False
+        Whether to return numerator and denominator of class_dis.
+
+    Returns
+    -------
+    class_dis : float
+        Class distinctiveness value
+    numerator : float
+        Numerator value of class_dis
+    denominator : float
+        Denominator value of class_dis
+
+    References
+    ----------
+    .. [1] `Defining and quantifying users’ mental imagery-based
+    BCI skills: a first step
+    <https://hal.archives-ouvertes.fr/hal-01846434/>`_
+    F. Lotte, and C. Jeunet. Journal of neural engineering,
+    15(4), 046030, 2018.
+    """
+
+    classes = np.unique(y)
+    if len(classes) <= 1:
+        raise ValueError('X must contain at least two classes')
+
+    elif len(classes) == 2:
+        # numerator computation
+        covmeans = [mean_riemann(X[y == ll]) for ll in classes]
+        numerator = distance_riemann(covmeans[0], covmeans[1])
+
+        # denominator computation
+        all_sigma = []
+        for ll in classes:
+            X_temp = X[y == ll]
+            dis_within = [distance_riemann(X_temp[r], covmeans[int(ll)])
+                          for r in range(len(X_temp))]
+            sigma = np.sum(dis_within) / len(X_temp)
+            all_sigma.append(sigma)
+        denominator = 0.5 * (np.sum(all_sigma))
+
+    else:
+        # numerator computation
+        covmeans = [mean_riemann(X[y == ll]) for ll in classes]
+        covmeans = np.array(covmeans)
+        ave_covmeans = mean_riemann(covmeans)
+        all_dis_between = [distance_riemann(covmeans[int(ll)],
+                                            ave_covmeans) for ll in classes]
+        numerator = np.sum(all_dis_between)
+
+        # denominator computation
+        all_sigma = []
+        for ll in classes:
+            X_temp = X[y == ll]
+            dis_within = [distance_riemann(X_temp[r], covmeans[int(ll)])
+                          for r in range(len(X_temp))]
+            sigma = np.sum(dis_within) / len(X_temp)
+            all_sigma.append(sigma)
+        denominator = np.sum(all_sigma)
+
+    class_dis = numerator / denominator
+
+    if return_num_denom:
+        return class_dis, numerator, denominator
+
+    else:
+        return class_dis

pyriemann/preprocessing.py
@@ -2,14 +2,12 @@ import numbers
 
 import numpy as np
 from scipy.linalg import eigh
-from itertools import combinations
 
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.extmath import stable_cumsum
 
-from .utils.mean import mean_covariance, mean_riemann
+from .utils.mean import mean_covariance
 from .utils.base import sqrtm, invsqrtm
-from .utils.distance import distance_riemann
 
 
 class Whitening(BaseEstimator, TransformerMixin):
@@ -202,63 +200,3 @@ class Whitening(BaseEstimator, TransformerMixin):
         """
         Xiw = self.inv_filters_.T @ X @ self.inv_filters_
         return Xiw
-
-
-def class_distinctiveness(X, y, nume_denomi=False):
-    """Measure class separability between classes on a manifold.
-
-        Parameters
-        ----------
-        X : ndarray, shape (n_matrices, n_channels, n_channels)
-            Set of SPD matrices.
-        y : ndarray, shape (n_matrices,)
-            Labels for each matrix.
-        nume_denomi : bool, default=False
-            Whether to return numerator and denominator of class_dis.
-
-        Returns
-        -------
-        class_dis : float
-            class distinctiveness value
-        numerator : float
-            Numerator value of class_dis
-        denominator : float
-            Denominator value of class_dis
-
-        References
-        ----------
-        .. [1] `Defining and quantifying users’ mental imagery-based
-        BCI skills: a first step
-        <https://hal.archives-ouvertes.fr/hal-01846434/>`_
-        F. Lotte, and C. Jeunet. Journal of neural engineering,
-        15(4), 046030, 2018.
-        """
-
-    classes = np.unique(y)
-
-    # numerator computation
-    all_dis_between = []
-    covmeans = [mean_riemann(X[y == ll]) for ll in classes]
-    for set in combinations(classes, 2):
-        dis_between = distance_riemann(covmeans[int(set[0])],
-                                       covmeans[int(set[1])])
-        all_dis_between.append(dis_between)
-    numerator = np.sum(all_dis_between)
-
-    # denominator computation
-    all_sigma = []
-    for ll in classes:
-        X_temp = X[y == ll]
-        dis_within = [distance_riemann(X_temp[r, :, :], covmeans[int(ll)])
-                      for r in range(len(X_temp))]
-        sigma = np.sum(dis_within) / len(X_temp)
-        all_sigma.append(sigma)
-    denominator = 0.5 * (np.sum(all_sigma))
-
-    class_dis = numerator / denominator
-
-    if not nume_denomi:
-        return class_dis
-
-    else:
-        return class_dis, numerator, denominator