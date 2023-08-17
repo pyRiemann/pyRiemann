b2618a91515bd6414bdad15745a1f071eee84cf6
qbarthelemy <q.barthelemy@gmail.com>
2023-08-17 22:54:15 +0200

    update release into deprecation msg

--- pyriemann/utils/mean.py
+++ pyriemann/utils/mean.py
@@ -14,7 +14,7 @@ from .utils import check_weights
 def _deprecate_covmats(covmats, X):
     if covmats is not None:
         print("DeprecationWarning: input covmats has been renamed into X and "
-              "will be removed in 0.7.0.")
+              "will be removed in 0.8.0.")
         X = covmats
     return X
 
