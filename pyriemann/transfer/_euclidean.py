94c67a7a64943e3a5608041354943a965a419c38
brunalopes <brunaafl@usp.br>
2024-04-04 14:22:12 -0300

    Organizing code

--- pyriemann/transfer/_euclidean.py
+++ pyriemann/transfer/_euclidean.py
@@ -1,5 +1,3 @@
-import copy
-
 import numpy as np
 
 from scipy.linalg import sqrtm, inv
@@ -12,6 +10,22 @@ from pyriemann.transfer import decode_domains
 
 
 def compute_ref_euclidean(data):
+    '''
+    Calculate the reference matrix for computing the Euclidean alignment,
+
+    Parameters
+    ----------
+    data: numpy array.
+    It can be either raw trials with shape (n, c, t) or covariances (n, c, c)
+    Reference trials to compute the alignment matrix
+
+    Returns
+    -------
+    r_ea: numpy array with shape (c, c)
+    Reference matrix
+
+    '''
+
     mean = mean_covariance(data, metric='euclid')
 
     compare = np.allclose(mean, np.identity(mean.shape[0]))
@@ -40,12 +54,54 @@ def compute_ref_euclidean(data):
 
 
 class TransformEA(BaseEstimator, TransformerMixin):
+    """Euclidean Alignment, based on [1].
 
+    The Euclidean Alignment is a transformation that recenter each domain's
+    Euclidean mean covariance matrix to the Identity, making distributions
+    more similar.
+
+    This transformation accept both raw signals and covariance matrices as
+    data inputs. For raw trials, you can determine the estimator using the
+    'estimtor' parameter.
+
+    .. note::
+       Using .fit() and then .transform() will give different results than
+       .fit_transform(). In fact, .fit_transform() should be applied on the
+       training dataset (target and source) and .transform() on the test
+       partition of the target dataset.
+
+    Parameters
+    ----------
+
+    dtype: str
+    Type of input data, it can be 'cov' for covariance or 'raw' for raw trials
+
+    estimator: str
+    If dtype=='raw', it represents the covariance estimator
+
+    Attributes
+    ----------
+    _ref : list
+        Contains the reference matrix for each domain.
+        Order of the list is the same as 'domains'
+
+    _cov : Dict
+        Dictionary containing the covariance matrices for each domain.
+        Order of the list is the same as 'domains'
+
+    References
+    ----------
+    .. [1] `Transfer Learning for Brain-Computer Interfaces:
+        A Euclidean Space Data Alignment Approach
+        <https://arxiv.org/abs/1808.05464>`_
+        He He and Dongrui Wu, IEEE Transactions on Biomedical Engineering, 2019
+
+    """
     def __init__(self, estimator='lwf', dtype='raw'):
         self.estimator = estimator
         self.dtype = dtype
         self._ref = list()
-        self._cov = list()
+        self._cov = {}
 
     def _compute_ref(self, X):
         ref = compute_ref_euclidean(X)
@@ -61,11 +117,13 @@ class TransformEA(BaseEstimator, TransformerMixin):
             X_d = X[domains == d]
 
             if self.dtype != 'covmat':
-                X_d = self._transform_cov(X_d)
+                X_d = self._transform_cov(X_d, d)
 
             self._compute_ref(X_d)
 
     def _transform_sw(self, X, domains):
+        """Align each domain"""
+
         X_align = []
         for i in range(len(np.unique(domains))):
 
@@ -82,10 +140,11 @@ class TransformEA(BaseEstimator, TransformerMixin):
         X_align = np.concatenate(X_align)
         return X_align
 
-    def _transform_cov(self, X):
+    def _transform_cov(self, X, d):
         """ Compute covariance matrices of trials in array X and save"""
+
         cov = covariances(X, estimator=self.estimator)
-        self._cov.append(cov)
+        self._cov[d] = cov
         return cov
 
     def fit(self, X, y_enc):
@@ -99,6 +158,9 @@ class TransformEA(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y_enc=None):
+        """
+        Used for transforming the target data after fitting the calibration
+        """
 
         # In this case, X is test and you suppose that calibration was previously fitted
         ref = self._ref[0]
@@ -111,6 +173,9 @@ class TransformEA(BaseEstimator, TransformerMixin):
         return X_align
 
     def fit_transform(self, X, y_enc=None, **fit_params):
+        """
+        Used for fitting and transforming 'train' data.
+        """
 
         self.fit(X, y_enc)
 
