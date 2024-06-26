d37ad34850762dc8fef6ed49923c8383aaf15005
qbarthelemy <q.barthelemy@gmail.com>
2024-06-26 22:05:11 +0200

    unpin sphinx

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     ],
     extras_require={
         "docs": [
-            "sphinx>=6.0.0,<=7.0.1",
+            "sphinx",
             "sphinx-gallery",
             "sphinx-bootstrap_theme",
             "numpydoc",
