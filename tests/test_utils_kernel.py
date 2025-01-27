b9f0a2442685ee3aaa75e507e766ee52c2b282c9
qbarthelemy <q.barthelemy@gmail.com>
2025-01-27 21:26:22 +0100

    add cref to kernel_logeuclid

--- tests/test_utils_kernel.py
+++ tests/test_utils_kernel.py
@@ -1,5 +1,9 @@
 import numpy as np
-from numpy.testing import assert_array_equal, assert_array_almost_equal
+from numpy.testing import (
+    assert_array_equal,
+    assert_array_almost_equal,
+    assert_raises,
+)
 import pytest
 
 from pyriemann.utils.base import logm
@@ -13,35 +17,23 @@ from pyriemann.utils.mean import mean_covariance
 from pyriemann.utils.test import is_sym_pos_semi_def as is_spsd
 
 rker_str = ["euclid", "logeuclid", "riemann"]
-rker_fct = [kernel_euclid, kernel_logeuclid, kernel_riemann]
-
-
-@pytest.mark.parametrize("ker", rker_fct)
-def test_kernel_x_x(ker, get_mats):
-    """Test kernel build"""
-    n_matrices, n_channels = 7, 3
-    X = get_mats(n_matrices, n_channels, "spd")
-    K = ker(X, X)
-    assert K.shape == (n_matrices, n_matrices)
-    assert is_spsd(K)
-    assert_array_almost_equal(K, ker(X))
 
 
 @pytest.mark.parametrize("ker", rker_str)
-def test_kernel_cref(ker, get_mats):
-    """Test kernel reference"""
+def test_kernel_x_x(ker, get_mats):
+    """Test kernel build"""
     n_matrices, n_channels = 5, 3
     X = get_mats(n_matrices, n_channels, "spd")
-    cref = mean_covariance(X, metric=ker)
     K = kernel(X, X, metric=ker)
-    K1 = kernel(X, X, Cref=cref, metric=ker)
-    assert_array_equal(K, K1)
+    assert K.shape == (n_matrices, n_matrices)
+    assert is_spsd(K)
+    assert_array_almost_equal(K, globals()[f"kernel_{ker}"](X))
 
 
 @pytest.mark.parametrize("ker", rker_str)
 def test_kernel_x_y(ker, get_mats):
     """Test kernel for different X and Y"""
-    n_matrices_X, n_matrices_Y, n_channels = 6, 5, 3
+    n_matrices_X, n_matrices_Y, n_channels = 4, 5, 3
     X = get_mats(n_matrices_X, n_channels, "spd")
     Y = get_mats(n_matrices_Y, n_channels, "spd")
     K = kernel(X, Y, metric=ker)
@@ -73,11 +65,33 @@ def test_input_dimension_error(ker, get_mats):
     X = get_mats(n_matrices, n_channels, "spd")
     Y = get_mats(n_matrices, n_channels + 1, "spd")
     cref = get_mats(1, n_channels + 1, "spd")[0]
-    if ker == "riemann":
+    if ker in ["logeuclid", "riemann"]:
         with pytest.raises(AssertionError):
             kernel(X, Cref=cref, metric=ker)
     with pytest.raises(AssertionError):
         kernel(X, Y, metric=ker)
+
+
+@pytest.mark.parametrize("n_channels", [4, 5, 6])
+@pytest.mark.parametrize("ker", rker_str)
+def test_cref(n_channels, ker, get_mats):
+    n_matrices_X, n_matrices_Y = 7, 4
+    X = get_mats(n_matrices_X, n_channels, "spd")
+    Y = get_mats(n_matrices_Y, n_channels, "spd")
+    K = kernel(X, Y, metric=ker)
+
+    if ker == "logeuclid":
+        Cref = np.eye(n_channels)
+    elif ker == "riemann":
+        Cref = mean_covariance(X)
+    else:
+        return
+
+    K1 = kernel(X, Y, Cref=Cref, metric=ker)
+    assert_array_equal(K, K1)
+
+    K2 = kernel(X, Y, Cref=X[0], metric=ker)
+    assert_raises(AssertionError, assert_array_equal, K, K2)
 
 
 @pytest.mark.parametrize("n_dim0, n_dim1", [(4, 4), (4, 5), (5, 4)])
@@ -87,7 +101,6 @@ def test_euclid(n_dim0, n_dim1, rndstate):
     X = rndstate.randn(n_matrices_X, n_dim0, n_dim1)
     Y = rndstate.randn(n_matrices_Y, n_dim0, n_dim1)
     K = kernel_euclid(X, Y)
-    assert K.shape == (n_matrices_X, n_matrices_Y)
 
     K1 = np.empty((n_matrices_X, n_matrices_Y))
     K2 = np.empty((n_matrices_X, n_matrices_Y))
@@ -97,6 +110,12 @@ def test_euclid(n_dim0, n_dim1, rndstate):
             K2[i, j] = np.dot(X[i].flatten(), Y[j].flatten())
     assert_array_almost_equal(K, K1)
     assert_array_almost_equal(K, K2)
+
+
+def test_logeuclid(get_mats):
+    n_matrices, n_channels = 5, 3
+    X = get_mats(n_matrices, n_channels, "spd")
+    kernel_logeuclid(X)
 
 
 def test_riemann_correctness(get_mats):
