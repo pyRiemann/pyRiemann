390ede2d4adaff789b71aaf71911340f042b9f82
toncho11 <toncho11@users.noreply.github.com>
2025-04-08 09:20:58 +0200

    Update pyriemann/utils/mean.py
    Co-authored-by: Quentin Barthélemy <q.barthelemy@gmail.com>

--- pyriemann/utils/mean.py
+++ pyriemann/utils/mean.py
@@ -481,7 +481,7 @@ def mean_power(X, p, *, sample_weight=None, zeta=10e-10, maxiter=100,
 
     if p == 1:
         return mean_euclid(X, sample_weight=sample_weight)
-    elif p == 0:
+    if p == 0:
         return mean_riemann(
                 X,
                 sample_weight=sample_weight,
