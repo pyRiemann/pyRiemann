2ed819a25e2aacee3f2c5efff151eaa1ce027736
Gregoire Cattan <gregoire.cattan@ibm.com>
2025-04-17 09:34:22 +0200

    Revert "Mean Field class update (#356)"
    This reverts commit 24f6afc67fd11811f11852bd84b60df8aa792e22.

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -121,11 +121,7 @@ def clf_transform(classif, mats, labels):
     n_matrices, n_classes = len(labels), len(np.unique(labels))
     clf = classif()
     transf = clf.fit(mats, labels).transform(mats)
-    if isinstance(clf, MeanField) and clf.method_label == "lda":
-        n_powers = len(clf.power_list)
-        assert transf.shape == (n_matrices, n_classes * n_powers)
-    else:
-        assert transf.shape == (n_matrices, n_classes)
+    assert transf.shape == (n_matrices, n_classes)
 
 
 def clf_fittransform(classif, mats, labels):
@@ -415,7 +411,7 @@ def test_svc_kernel_error(get_mats, get_labels):
 
 
 @pytest.mark.parametrize("power_list", [[-1, 0, 1], [0, 0.1]])
-@pytest.mark.parametrize("method_label", ["lda", "sum_means", "inf_means"])
+@pytest.mark.parametrize("method_label", ["sum_means", "inf_means"])
 @pytest.mark.parametrize("metric", get_distances())
 def test_meanfield(get_mats, get_labels, power_list, method_label, metric):
     n_powers = len(power_list)
@@ -436,10 +432,7 @@ def test_meanfield(get_mats, get_labels, power_list, method_label, metric):
     proba = mf.predict_proba(mats)
     assert proba.shape == (n_matrices, n_classes)
     transf = mf.transform(mats)
-    if method_label == "sum_means" or method_label == "inf_means":
-        assert transf.shape == (n_matrices, n_classes)
-    elif method_label == "lda":
-        assert transf.shape == (n_matrices, n_classes * n_powers)
+    assert transf.shape == (n_matrices, n_classes)
 
 
 @pytest.mark.parametrize("kind", ["spd", "hpd"])
