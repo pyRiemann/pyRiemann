0ecd0a972f9628ab504a1d0174b0a50035b741c3
qbarthelemy <q.barthelemy@gmail.com>
2025-05-20 00:49:13 +0200

    correct flake8

--- pyriemann/utils/utils.py
+++ pyriemann/utils/utils.py
@@ -154,8 +154,8 @@ def check_function(fun, functions):
         If string, it must be one of the keys of ``functions``.
         If callable, it can be a function defined in API or by the user.
         In the latter case, the signature of the function as to match the ones
-        defined in ``functions``. This is the user responsibility to ensure this,
-        and will not be checked.
+        defined in ``functions``. This is the user responsibility to ensure
+        this, and will not be checked.
     functions : dict
         Functions available in API, used only when ``fun`` is a string.
 
