correct formula

pyriemann/utils/tangentspace.py
@@ -339,8 +339,7 @@ def log_map_logchol(X, Cref):
         <https://arxiv.org/pdf/1908.09326>`_
         Z. Lin. SIAM J Matrix Anal Appl, 2019, 40(4), pp. 1353-1370.
     """
-    X_chol = np.linalg.cholesky(X)
-    Cref_chol = np.linalg.cholesky(Cref)
+    X_chol, Cref_chol = np.linalg.cholesky(X), np.linalg.cholesky(Cref)
 
     res = np.zeros_like(X)
 
@@ -351,7 +350,7 @@ def log_map_logchol(X, Cref):
     res[..., diag0, diag1] = Cref_chol[..., diag0, diag1] * \
         np.log(X_chol[..., diag0, diag1] / Cref_chol[..., diag0, diag1])
 
-    X_new = Cref_chol @ ctranspose(res) + res @ ctranspose(Cref_chol)
+    X_new = Cref_chol @ ctranspose(res) + res @ Cref_chol.conj().T
 
     return X_new
 
@@ -723,13 +722,7 @@ def transport_logchol(X, A, B):
     The parallel transport of matrices :math:`\mathbf{X}` in tangent space
     from an initial SPD/HPD matrix :math:`\mathbf{A}` to a final SPD/HPD
     matrix :math:`\mathbf{B}` for log-Cholesky metric is given in Proposition 7
-    of [1]_:
-
-    .. math::
-        \mathbf{X}_\text{new} = \text{lower}(\mathbf{X}) +
-        \text{diag}(\text{chol}(\mathbf{B}))
-        \text{diag}(\text{chol}(\mathbf{A}))^{-1}
-        \text{diag}(\text{chol}(\mathbf{X}))
+    of [1]_.
 
     Warning: this function must be applied to matrices :math:`\mathbf{X}`
     already projected in tangent space with a logarithmic map at
@@ -765,16 +758,23 @@ def transport_logchol(X, A, B):
         Z. Lin. SIAM J Matrix Anal Appl, 2019, 40(4), pp. 1353-1370.
     """
     A_chol, B_chol = np.linalg.cholesky(A), np.linalg.cholesky(B)
-
-    X_new = np.zeros_like(X)
+    A_invchol = np.linalg.inv(A_chol)
 
     tri0, tri1 = np.tril_indices(X.shape[-1], -1)
-    X_new[..., tri0, tri1] = X[..., tri0, tri1]
-
     diag0, diag1 = np.diag_indices(X.shape[-1])
-    X_new[..., diag0, diag1] = B_chol[..., diag0, diag1] \
-        / A_chol[..., diag0, diag1] * X[..., diag0, diag1]
 
+    P = A_invchol @ X @ A_invchol.T
+    P12 = np.zeros_like(P)
+    P12[..., tri0, tri1] = P[..., tri0, tri1]
+    P12[..., diag0, diag1] = P[..., diag0, diag1] / 2
+    X_ = A_chol @ P12
+
+    T = np.zeros_like(X)
+    T[..., tri0, tri1] = X_[..., tri0, tri1]
+    T[..., diag0, diag1] = B_chol[..., diag0, diag1] \
+        / A_chol[..., diag0, diag1] * X_[..., diag0, diag1]
+
+    X_new = B_chol @ ctranspose(T) + T @ B_chol.conj().T
     return X_new
 
 
@@ -928,6 +928,7 @@ def transport(X, A, B, metric="riemann"):
     See Also
     --------
     transport_euclid
+    transport_logchol
     transport_logeuclid
     transport_riemann
     """